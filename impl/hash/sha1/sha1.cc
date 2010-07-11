5cc4c65f0ebb7b1118a4e4f3e769cb4b30e57e6a
brian m. carlson <sandals@crustytoothpaste.ath.cx>
2010-07-11 17:07:59 +0000

    Add SHA-0.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.ath.cx>

--- impl/hash/sha1/sha1.cc
+++ impl/hash/sha1/sha1.cc
@@ -4,9 +4,8 @@
  * it.  However, a credit in the documentation, although not required, would be
  * appreciated.
  */
-/* This code implements the SHA1 and SHA0 message digest algorithms.  It is
- * compatible with OpenBSD's implementation.  The size of the SHA1_CTX struct is
- * not guaranteed compatible, however.  This implementation requires ANSI C.
+/* This code implements the SHA1 and SHA0 message digest algorithms.  This
+ * implementation requires ISO C++.
  */
 
 #include <stdio.h>
@@ -20,8 +19,10 @@
 
 extern "C" {
 PLUGIN_STRUCTURE(sha1, drew::SHA1)
+PLUGIN_STRUCTURE(sha0, drew::SHA0)
 PLUGIN_DATA_START()
-PLUGIN_DATA(sha1, "SHA1")
+PLUGIN_DATA(sha1, "SHA-1")
+PLUGIN_DATA(sha0, "SHA-0")
 PLUGIN_DATA_END()
 PLUGIN_INTERFACE()
 }
@@ -49,7 +50,7 @@ static inline uint32_t ROL(uint32_t x, int n)
 	return ((x<<n)|(x>>(32-n)));
 }
 
-drew::SHA1::SHA1()
+drew::SHA::SHA()
 {
 	m_hash[0] = 0x67452301;
 	m_hash[1] = 0xefcdab89;
@@ -62,13 +63,14 @@ drew::SHA1::SHA1()
 #define OP(f, g, a, b, c, d, e) \
 	e+=ROL(a, 5)+f(b, c, d)+g; b=ROL(b, 30);
 #define EXPANSION(i) \
-	(blk[(i)&15]=ROL(blk[((i)+13)&15]^blk[((i)+8)&15]^blk[((i)+2)&15]^blk[(i)&15],1))
+	(blk[(i)&15]=ROL(blk[((i)+13)&15]^blk[((i)+8)&15]^blk[((i)+2)&15]^blk[(i)&15],Rotate))
 
 /* This implementation uses a circular buffer to create the expansions of blk.
  * While it appears that this would be slower, it instead is significantly
  * faster (174 MiB/s vs. 195 MiB/s).
  */
-void drew::SHA1::Transform(quantum_t *state, const uint8_t *block)
+template<int Rotate>
+void drew::SHA::Transform(quantum_t *state, const uint8_t *block)
 {
 	uint32_t blk[block_size/sizeof(uint32_t)];
 	size_t i;
@@ -99,30 +101,30 @@ void drew::SHA1::Transform(quantum_t *state, const uint8_t *block)
 	OP(ff, blk[13], c, d, e, a, b);
 	OP(ff, blk[14], b, c, d, e, a);
 	OP(ff, blk[15], a, b, c, d, e);
-	OP(ff, EXPANSION(16), e, a, b, c, d);
-	OP(ff, EXPANSION(17), d, e, a, b, c);
-	OP(ff, EXPANSION(18), c, d, e, a, b);
-	OP(ff, EXPANSION(19), b, c, d, e, a);
+	OP(ff, EXPANSION1(16), e, a, b, c, d);
+	OP(ff, EXPANSION1(17), d, e, a, b, c);
+	OP(ff, EXPANSION1(18), c, d, e, a, b);
+	OP(ff, EXPANSION1(19), b, c, d, e, a);
 	for (i=20; i<40; i+=5) {
-		OP(gg, EXPANSION(i  ), a, b, c, d, e);
-		OP(gg, EXPANSION(i+1), e, a, b, c, d);
-		OP(gg, EXPANSION(i+2), d, e, a, b, c);
-		OP(gg, EXPANSION(i+3), c, d, e, a, b);
-		OP(gg, EXPANSION(i+4), b, c, d, e, a);
+		OP(gg, EXPANSION1(i  ), a, b, c, d, e);
+		OP(gg, EXPANSION1(i+1), e, a, b, c, d);
+		OP(gg, EXPANSION1(i+2), d, e, a, b, c);
+		OP(gg, EXPANSION1(i+3), c, d, e, a, b);
+		OP(gg, EXPANSION1(i+4), b, c, d, e, a);
 	}
 	for (i=40; i<60; i+=5) {
-		OP(hh, EXPANSION(i  ), a, b, c, d, e);
-		OP(hh, EXPANSION(i+1), e, a, b, c, d);
-		OP(hh, EXPANSION(i+2), d, e, a, b, c);
-		OP(hh, EXPANSION(i+3), c, d, e, a, b);
-		OP(hh, EXPANSION(i+4), b, c, d, e, a);
+		OP(hh, EXPANSION1(i  ), a, b, c, d, e);
+		OP(hh, EXPANSION1(i+1), e, a, b, c, d);
+		OP(hh, EXPANSION1(i+2), d, e, a, b, c);
+		OP(hh, EXPANSION1(i+3), c, d, e, a, b);
+		OP(hh, EXPANSION1(i+4), b, c, d, e, a);
 	}
 	for (i=60; i<80; i+=5) {
-		OP(ii, EXPANSION(i  ), a, b, c, d, e);
-		OP(ii, EXPANSION(i+1), e, a, b, c, d);
-		OP(ii, EXPANSION(i+2), d, e, a, b, c);
-		OP(ii, EXPANSION(i+3), c, d, e, a, b);
-		OP(ii, EXPANSION(i+4), b, c, d, e, a);
+		OP(ii, EXPANSION1(i  ), a, b, c, d, e);
+		OP(ii, EXPANSION1(i+1), e, a, b, c, d);
+		OP(ii, EXPANSION1(i+2), d, e, a, b, c);
+		OP(ii, EXPANSION1(i+3), c, d, e, a, b);
+		OP(ii, EXPANSION1(i+4), b, c, d, e, a);
 	}
 
 	state[0] += a;
--- impl/hash/sha1/sha1.hh
+++ impl/hash/sha1/sha1.hh
@@ -7,12 +7,45 @@
 
 namespace drew {
 
-class SHA1 : public Hash<uint32_t, 20, 20, 64, BigEndian>
+class SHA : public Hash<uint32_t, 20, 20, 64, BigEndian> 
 {
 	public:
-		SHA1();
-		virtual ~SHA1() {}
+		SHA();
+		virtual ~SHA() {}
+		template<int Rotate>
 		static void Transform(quantum_t *state, const uint8_t *data);
+	protected:
+		virtual void Transform(const uint8_t *data) = 0;
+	private:
+};
+
+// The static Transform functions in the derived classes are required because of
+// the way the automatic plugin-registration code works.  At some point in the
+// future, the code will be reworked to avoid that problem altogether.
+class SHA1 : public SHA
+{
+	public:
+		virtual ~SHA1() {}
+		static void Transform(quantum_t *state, const uint8_t *data)
+		{
+			SHA::Transform<1>(state, data);
+		}
+	protected:
+		virtual void Transform(const uint8_t *data)
+		{
+			Transform(m_hash, data);
+		}
+	private:
+};
+
+class SHA0 : public SHA
+{
+	public:
+		virtual ~SHA0() {}
+		static void Transform(quantum_t *state, const uint8_t *data)
+		{
+			SHA::Transform<0>(state, data);
+		}
 	protected:
 		virtual void Transform(const uint8_t *data)
 		{
