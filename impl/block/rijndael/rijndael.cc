d9148d114db7e1ce67374aaf07a3400dbc3e317d
brian m. carlson <sandals@crustytoothpaste.ath.cx>
2010-08-05 12:55:06 +0000

    Add additional test for Rijndael.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.ath.cx>

--- impl/block/rijndael/rijndael.cc
+++ impl/block/rijndael/rijndael.cc
@@ -171,12 +171,35 @@ static int rd_test(void *)
 		"000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f";
 	res |= !test(key, "00112233445566778899aabbccddeeff",
 			"69c4e0d86a7b0430d8cdb78070b4c55a", 16);
-
 	res |= !test(key, "00112233445566778899aabbccddeeff",
 			"dda97ca4864cdfe06eaf70a0ec0d7191", 24);
-
 	res |= !test(key, "00112233445566778899aabbccddeeff",
 			"8ea2b7ca516745bfeafc49904b496089", 32);
+	res <<= 1;
+	const char *key2 = "2b7e151628aed2a6abf7158809cf4f3c";
+	const char *pt1 = "6bc1bee22e409f96e93d7e117393172a";
+	const char *pt2 = "ae2d8a571e03ac9c9eb76fac45af8e51";
+	const char *pt3 = "30c81c46a35ce411e5fbc1191a0a52ef";
+	const char *pt4 = "f69f2445df4f9b17ad2b417be66c3710";
+	res |= !test(key2, pt1, "3ad77bb40d7a3660a89ecaf32466ef97");
+	res |= !test(key2, pt2, "f5d3d58503b9699de785895a96fdbaaf");
+	res |= !test(key2, pt3, "43b1cd7f598ece23881b00e3ed030688");
+	res |= !test(key2, pt4, "7b0c785e27e8ad3f8223207104725dd4");
+	res <<= 1;
+	const char *key3 = "8e73b0f7da0e6452c810f32b809079e562f8ead2522c6b7b";
+	res |= !test(key3, pt1, "bd334f1d6e45f25ff712a214571fa5cc");
+	res |= !test(key3, pt2, "974104846d0ad3ad7734ecb3ecee4eef");
+	res |= !test(key3, pt3, "ef7afd2270e2e60adce0ba2face6444e");
+	res |= !test(key3, pt4, "9a4b41ba738d6c72fb16691603c18e0e");
+	res <<= 1;
+	const char *key4 =
+		"603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4";
+	res |= !test(key4, pt1, "f3eed1bdb5d2a03c064b5a7e3db181f8");
+	res |= !test(key4, pt2, "591ccb10d410ed26dc5ba74a31362870");
+	res |= !test(key4, pt3, "b6ed21b99ca6f4f9f153e7b1beafed1d");
+	res |= !test(key4, pt4, "23304b7a39f9f3ff067d8d8f9e24ecc7");
+	res <<= 1;
+
 
 	return 0;
 }
