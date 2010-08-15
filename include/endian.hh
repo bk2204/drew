9604dc8c692babffbc5248a0a2ccc05206e4f022
brian m. carlson <sandals@crustytoothpaste.ath.cx>
2010-08-15 23:35:32 +0000

    endian.hh: split out 3-argument Copy functions.
    It may be useful to specialize the 3-argument versions independently of the
    4-argument versions, since the former are trivially implementable with e.g.
    htonl.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.ath.cx>

--- include/endian.hh
+++ include/endian.hh
@@ -9,8 +9,18 @@ class BigEndian
 {
 	public:
 		template<class T>
+		static void Copy(uint8_t *dest, const T *src, size_t len)
+		{
+			Copy(dest, src, len, sizeof(T));
+		}
+		template<class T>
+		static void Copy(T *dest, const uint8_t *src, size_t len)
+		{
+			Copy(dest, src, len, sizeof(T));
+		}
+		template<class T>
 		static void Copy(uint8_t *dest, const T *src, size_t len,
-				const size_t sz = sizeof(T))
+				const size_t sz)
 		{
 			memcpy(dest, src, len);
 #if BYTE_ORDER == LITTLE_ENDIAN
@@ -23,7 +33,7 @@ class BigEndian
 		}
 		template<class T>
 		static void Copy(T *dest, const uint8_t *src, size_t len,
-				const size_t sz = sizeof(T))
+				const size_t sz)
 		{
 			memcpy(dest, src, len);
 #if BYTE_ORDER == LITTLE_ENDIAN
@@ -42,8 +52,18 @@ class LittleEndian
 {
 	public:
 		template<class T>
+		static void Copy(uint8_t *dest, const T *src, size_t len)
+		{
+			Copy(dest, src, len, sizeof(T));
+		}
+		template<class T>
+		static void Copy(T *dest, const uint8_t *src, size_t len)
+		{
+			Copy(dest, src, len, sizeof(T));
+		}
+		template<class T>
 		static void Copy(uint8_t *dest, const T *src, size_t len,
-				const size_t sz = sizeof(T))
+				const size_t sz)
 		{
 			memcpy(dest, src, len);
 #if BYTE_ORDER == BIG_ENDIAN
@@ -56,7 +76,7 @@ class LittleEndian
 		}
 		template<class T>
 		static void Copy(T *dest, const uint8_t *src, size_t len,
-				const size_t sz = sizeof(T))
+				const size_t sz)
 		{
 			memcpy(dest, src, len);
 #if BYTE_ORDER == BIG_ENDIAN
