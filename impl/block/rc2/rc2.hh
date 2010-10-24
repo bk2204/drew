52f6e7dea3885d895320f245f2ebb56e06ccdd6b
brian m. carlson <sandals@crustytoothpaste.net>
2010-10-24 19:36:53 +0000

    rc2: make more functions inline.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- impl/block/rc2/rc2.hh
+++ impl/block/rc2/rc2.hh
@@ -21,14 +21,14 @@ class RC2
 		void Decrypt(uint8_t *out, const uint8_t *in);
 	protected:
 	private:
-		void Mix(uint16_t *r, size_t i, size_t j, size_t s);
-		void MixRound(uint16_t *r, size_t j);
-		void Mash(uint16_t *r, size_t i);
-		void MashRound(uint16_t *r);
-		void RMix(uint16_t *r, size_t i, size_t j, size_t s);
-		void RMixRound(uint16_t *r, size_t j);
-		void RMash(uint16_t *r, size_t i);
-		void RMashRound(uint16_t *r);
+		inline void Mix(uint16_t *r, size_t i, size_t j, size_t s);
+		inline void MixRound(uint16_t *r, size_t j);
+		inline void Mash(uint16_t *r, size_t i);
+		inline void MashRound(uint16_t *r);
+		inline void RMix(uint16_t *r, size_t i, size_t j, size_t s);
+		inline void RMixRound(uint16_t *r, size_t j);
+		inline void RMash(uint16_t *r, size_t i);
+		inline void RMashRound(uint16_t *r);
 		uint16_t m_k[64];
 		static const uint8_t pitable[];
 
