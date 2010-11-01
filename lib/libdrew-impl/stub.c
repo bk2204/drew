aabf4905885fb1c2c11319f6e134fafef8ab7a5c
brian m. carlson <sandals@crustytoothpaste.net>
2010-11-01 23:21:23 +0000

    Add support for libdrew-impl.so.
    libdrew-impl provides a different way to load algorithms when plugins are not
    suitable.  For example, profiling with gprof does not work with plugins.  It
    still requires the use of the dynamic loader, and therefore libdl.so, but
    because it is a shared library, -rdynamic is not required.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

---
+++ lib/libdrew-impl/stub.c
@@ -0,0 +1,25 @@
+#include <internal.h>
+#include <plugin.h>
+
+#include <stddef.h>
+#include <errno.h>
+
+#include "modules.gen"
+
+#define DIM(x) (sizeof(x)/sizeof((x)[0]))
+
+int drew_plugin_info(void *ldr, int op, int id, void *p)
+{
+	int plugcnt = 0;
+	for (size_t i = 0; i < DIM(plugin_list); i++) {
+		int nplugins = plugin_list[i](ldr, DREW_LOADER_GET_NPLUGINS, 0, NULL);
+		if ((op != DREW_LOADER_GET_NPLUGINS) && (id < (nplugins + plugcnt))) {
+			return plugin_list[i](ldr, op, id-plugcnt, p);
+		}
+		else
+			plugcnt += nplugins;
+	}
+	if (op == DREW_LOADER_GET_NPLUGINS)
+		return plugcnt;
+	return -EINVAL;
+}
