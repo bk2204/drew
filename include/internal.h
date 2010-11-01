53779b9e7216b6d11574d3706993e3800001fb89
brian m. carlson <sandals@crustytoothpaste.net>
2010-11-01 23:35:28 +0000

    internal.h: helper to use the right symbol name.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- include/internal.h
+++ include/internal.h
@@ -6,4 +6,12 @@
 #define _BSD_SOURCE 1
 #define DREW_IN_BUILD 1
 
+#if defined(DREW_AS_PLUGIN)
+#define DREW_PLUGIN_NAME(x) drew_plugin_info
+#elif defined(DREW_AS_MODULE)
+#define DREW_PLUGIN_NAME(x) drew_plugin_info_ ## x
+#endif
+
+typedef int (*drew_plugin_api_t)(void *, int, int, void *);
+
 #endif
