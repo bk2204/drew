b0d53737351817add6fe0bcf8c46685d2e865938
brian m. carlson <sandals@crustytoothpaste.net>
2011-02-05 21:24:03 +0000

    Compile with POSIX 1003.1-2001 by default.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- include/internal.h
+++ include/internal.h
@@ -1,8 +1,12 @@
 #ifndef DREW_INTERNAL_H
 #define DREW_INTERNAL_H
 
+#ifndef _POSIX_C_SOURCE
 #define _POSIX_C_SOURCE 200809L
+#endif
+#ifndef _XOPEN_SOURCE
 #define _XOPEN_SOURCE 700
+#endif
 #define _BSD_SOURCE 1
 #define DREW_IN_BUILD 1
 
