81b6fc39a0015b5c66bc2896809d0111a74b4bf1
brian m. carlson <sandals@crustytoothpaste.net>
2011-03-02 00:05:17 +0000

    plugin-main: handle multiple plugins.
    This commit adds support for specifying multiple plugins on the command line at
    once.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- test/plugin-main.c
+++ test/plugin-main.c
@@ -17,41 +17,47 @@ int main(int argc, char **argv)
 {
 	drew_loader_t *ldr = NULL;
 	int retval = 0;
-	int size = 0;
-	int id = 0;
-	int type = 0, nplugins = 0;
-	const void *tbl = NULL;
 
 	if (argc < 2)
 		return 2;
 
 	if ((retval = drew_loader_new(&ldr)))
 		errprint(retval, 3);
-	if ((id = drew_loader_load_plugin(ldr, argv[1], NULL)) < 0)
-		errprint(id, 4);
-	if ((nplugins = drew_loader_get_nplugins(ldr, id)) < 0)
-		errprint(nplugins, 6);
 
-	printf("Loaded plugin %s (%d implementations).\n", argv[1], nplugins);
-	for (int i = 0; i < nplugins; i++) {
-		const char *name;
-		drew_metadata_t md;
+	for (int j = 1; j < argc; j++) {
+		int size = 0;
+		int id = 0;
+		int type = 0, nplugins = 0;
+		const void *tbl = NULL;
 
-		if ((size = drew_loader_get_functbl(ldr, i, &tbl)) < 0)
-			errprint(size, 5);
-		if ((type = drew_loader_get_type(ldr, i)) < 0)
-			errprint(type, 7);
-		if ((retval = drew_loader_get_algo_name(ldr, i, &name)) < 0)
-			errprint(retval, 8);
+		if ((id = drew_loader_load_plugin(ldr, argv[j], NULL)) < 0)
+			errprint(id, 4);
+		if ((nplugins = drew_loader_get_nplugins(ldr, id)) < 0)
+			errprint(nplugins, 6);
 
-		printf("  Plugin %s (id %d of %d).\n", name, i, nplugins);
-		printf("    Type is %#x.\n", type);
-		printf("    Function table is at %p (%d bytes).\n", tbl, size);
+		printf("Loaded plugin %s (%d implementations).\n", argv[j], nplugins);
+		for (int i = 0; i < nplugins; i++) {
+			const char *name;
+			drew_metadata_t md;
+			const int implid = id + i;
 
-		for (int item = 0; !drew_loader_get_metadata(ldr, i, item, &md);
-				item++) {
-			printf("    Metadata: %s %s (%d)\n", md.predicate, md.object,
-					md.type);
+			if ((size = drew_loader_get_functbl(ldr, implid, &tbl)) < 0)
+				errprint(size, 5);
+			if ((type = drew_loader_get_type(ldr, implid)) < 0)
+				errprint(type, 7);
+			if ((retval = drew_loader_get_algo_name(ldr, implid, &name)) < 0)
+				errprint(retval, 8);
+
+			printf("  Plugin %s (id %d; implementation %d of %d).\n", name,
+					implid, i, nplugins);
+			printf("    Type is %#x.\n", type);
+			printf("    Function table is at %p (%d bytes).\n", tbl, size);
+
+			for (int item = 0;
+					!drew_loader_get_metadata(ldr, implid, item, &md); item++) {
+				printf("    Metadata: %s %s (%d)\n", md.predicate, md.object,
+						md.type);
+			}
 		}
 	}
 	if (drew_loader_free(&ldr))
