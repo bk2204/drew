600817c3943307def139173e68928b1b277f3eab
brian m. carlson <sandals@crustytoothpaste.net>
2011-03-20 21:52:23 +0000

    hash/testcase: allow variable-length hashes.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- impl/hash/testcase.hh
+++ impl/hash/testcase.hh
@@ -8,40 +8,59 @@
 
 namespace drew {
 
-template<class T>
+template<class T, unsigned N>
+struct HashInstantiator
+{
+	static T *CreateInstance()
+	{
+		return new T;
+	}
+};
+
+template<class T, unsigned N>
+struct VariableSizedHashInstantiator
+{
+	static T *CreateInstance()
+	{
+		return new T(N);
+	}
+};
+
+template<class T, unsigned N = T::digest_size,
+	class I = HashInstantiator<T, N> >
 class HashTestCase
 {
 	public:
-		HashTestCase(const uint8_t *buf, size_t len, size_t reps) :
-			m_buf(buf), m_len(len), m_reps(reps)
+		HashTestCase(const uint8_t *buf, size_t len, size_t reps)
 		{
+			Initialize(buf, len, reps);
 		}
 		HashTestCase(const char *buf, size_t reps)
 		{
-			m_buf = reinterpret_cast<const uint8_t *>(buf);
-			m_len = strlen(buf);
-			m_reps = reps;
+			Initialize(reinterpret_cast<const uint8_t *>(buf), strlen(buf),
+					reps);
 		}
-		~HashTestCase() {};
+		virtual ~HashTestCase() {};
 		bool Test(const uint8_t *buf, size_t len)
 		{
-			if (len != T::digest_size)
+			if (len != N)
 				return false;
 
-			T hash;
+			T *hash = CreateInstance();
 			for (size_t i = 0; i < m_reps; i++)
-				hash.Update(m_buf, m_len);
-			hash.GetDigest(m_result, 0);
+				hash->Update(m_buf, m_len);
+			hash->GetDigest(m_result, 0);
 
-			int result = !memcmp(buf, m_result, T::digest_size);
+			int result = !memcmp(buf, m_result, N);
+			delete hash;
 			return result;
 		}
 		bool Test(const char *str)
 		{
-			uint8_t buf[T::digest_size];
+			uint8_t buf[N];
 			const size_t len = strlen(str);
 
-			if (len != (T::digest_size * 2))
+			if (len != (N * 2))
 				return false;
 
 			StringToBytes(buf, str, len/2);
@@ -60,34 +79,47 @@ class HashTestCase
 #define NBYTEVALS 256
 		static bool MaintenanceTest(const uint8_t *output)
 		{
-			T context;
+			T *context = CreateInstance();
 			uint8_t buf[NBYTEVALS][256];
-			T *ctxt = new T[NBYTEVALS];
+			T *ctxt[NBYTEVALS];
 
 			for (size_t i = 0; i < NBYTEVALS; i++) {
 				memset(buf[i], (uint8_t)i, 256);
-				ctxt[i].Update(buf[i], i);
+				ctxt[i] = CreateInstance();
+				ctxt[i]->Update(buf[i], i);
 			}
 
 			for (size_t i = 0; i < 50000; i++) {
 				const uint8_t imod = i;
-				uint8_t md[T::digest_size];
+				uint8_t md[N];
 
-				T clone(ctxt[imod]);
+				T clone(*ctxt[imod]);
 				clone.GetDigest(md, false);
-				context.Update(md, T::digest_size);
-				ctxt[imod].Update(buf[imod], 256);
+				context->Update(md, N);
+				ctxt[imod]->Update(buf[imod], 256);
 			}
-			uint8_t md[T::digest_size];
-			context.GetDigest(md, false);
+			for (size_t i = 0; i < NBYTEVALS; i++)
+				delete ctxt[i];
+			uint8_t md[N];
+			context->GetDigest(md, false);
+			delete context;
 
-			bool res = !memcmp(md, output, T::digest_size);
-
-			delete[] ctxt;
+			bool res = !memcmp(md, output, N);
 
 			return res;
 		}
 	protected:
+		HashTestCase() {}
+		static T * CreateInstance()
+		{
+			return I::CreateInstance();
+		}
+		void Initialize(const uint8_t *buf, size_t len, size_t reps)
+		{
+			m_buf = buf;
+			m_len = len;
+			m_reps = reps;
+		}
 		static uint8_t *StringToBytes(const char *str, size_t len)
 		{
 			uint8_t *buf = new uint8_t[len];
@@ -107,10 +139,26 @@ class HashTestCase
 			}
 			return true;
 		}
-	private:
 		const uint8_t *m_buf;
 		size_t m_len, m_reps;
-		uint8_t m_result[T::digest_size];
+		uint8_t m_result[N];
+};
+
+template<class T, unsigned N>
+class VariableSizedHashTestCase :
+	public HashTestCase<T, N, VariableSizedHashInstantiator<T, N> >
+{
+	public:
+		VariableSizedHashTestCase(const uint8_t *buf, size_t len, size_t reps)
+		{
+			this->Initialize(buf, len, reps);
+		}
+		VariableSizedHashTestCase(const char *buf, size_t reps)
+		{
+			this->Initialize(reinterpret_cast<const uint8_t *>(buf),
+					strlen(buf), reps);
+		}
+		virtual ~VariableSizedHashTestCase() {};
 };
 
 }
