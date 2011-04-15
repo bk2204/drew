b1e0e95bdffa246a599017d142a69c07fbedbd34
brian m. carlson <sandals@crustytoothpaste.net>
2011-04-15 21:11:38 +0000

    test-hash: implement external tests.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- test/test-hash.c
+++ test/test-hash.c
@@ -18,6 +18,8 @@
 #include <drew/plugin.h>
 #include <drew/hash.h>
 
+#define FILENAME "test/vectors-hash"
+
 int test_get_type(void)
 {
 	return DREW_TYPE_HASH;
@@ -50,7 +52,125 @@ inline int test_speed_loop(drew_hash_t *ctx, uint8_t *buf,
 	return i;
 }
 
-#include "stubs.c"
+struct testcase {
+	char *id;
+	char *algo;
+	size_t nrepeats;
+	size_t insize;
+	uint8_t *in;
+	size_t digestsize;
+	uint8_t *digest;
+};
+
+const char *test_get_filename()
+{
+	return FILENAME;
+}
+
+void test_reset_data(void *p, int flags)
+{
+	struct testcase *tc = p;
+	if (flags & TEST_RESET_PARTIAL) {
+		free(tc->id);
+		tc->id = NULL;
+	}
+	if (flags & TEST_RESET_FREE) {
+		free(tc->id);
+		free(tc->algo);
+		free(tc->in);
+		free(tc->digest);
+		memset(p, 0, sizeof(struct testcase));
+	}
+	if (flags & TEST_RESET_ZERO)
+		memset(p, 0, sizeof(struct testcase));
+}
+
+void *test_create_data()
+{
+	void *p = malloc(sizeof(struct testcase));
+	test_reset_data(p, TEST_RESET_ZERO);
+	return p;
+}
+
+char *test_get_id(void *data)
+{
+	struct testcase *tc = data;
+	return strdup(tc->id);
+}
+
+int test_execute(void *data, const char *name, const void *tbl,
+		struct test_external *tep)
+{
+	int result = 0;
+	struct testcase *tc = data;
+	// If the test isn't for us or is corrupt, we succeed since it isn't
+	// relevant for our case.
+	if (!tc->algo)
+		return TEST_CORRUPT;
+	if (strcmp(name, tc->algo))
+		return TEST_NOT_FOR_US;
+	size_t len = tc->digestsize;
+	if (!tc->in || !tc->digest)
+		return TEST_CORRUPT;
+	uint8_t *buf = malloc(len);
+
+	drew_param_t param;
+	param.name = "digestSize";
+	param.param.number = tc->digestsize;
+	param.next = NULL;
+	drew_hash_t ctx;
+	ctx.functbl = tbl;
+	ctx.functbl->init(&ctx, 0, tep->ldr, &param);
+	for (size_t i = 0; i <= tc->nrepeats; i++)
+		ctx.functbl->update(&ctx, tc->in, tc->insize);
+	ctx.functbl->final(&ctx, buf, 0);
+	ctx.functbl->fini(&ctx, 0);
+	if (memcmp(buf, tc->digest, len))
+		result = TEST_FAILED;
+
+	free(buf);
+	return result;
+}
+
+int test_process_testcase(void *data, int type, const char *item,
+		struct test_external *tep)
+{
+	struct testcase *tc = data;
+
+	switch (type) {
+		case 'T':
+			if (!tc->id)
+				tc->id = strdup(item);
+			else if (strcmp(tc->id, item))
+				return TEST_EXECUTE;
+			break;
+		case 'a':
+			free(tc->algo);
+			tc->algo = strdup(item);
+			break;
+		case 'r':
+			if (sscanf(item, "%zu", &tc->nrepeats) != 1)
+				return TEST_CORRUPT;
+			break;
+		case 'C':
+			if (sscanf(item, "%zu", &tc->digestsize) != 1)
+				return TEST_CORRUPT;
+			break;
+		case 'p':
+			tc->insize = strlen(item) / 2;
+			if (process_bytes(tc->insize, &tc->in, item))
+				return TEST_CORRUPT;
+			break;
+		case 'c':
+			if (!tc->digestsize)
+				tc->digestsize = strlen(item) / 2;
+			if (process_bytes(tc->digestsize, &tc->digest, item))
+				return TEST_CORRUPT;
+			break;
+	}
+
+	return TEST_OK;
+}
 
 int test_speed(drew_loader_t *ldr, const char *name, const char *algo,
 		const void *tbl, int chunk, int nchunks)
