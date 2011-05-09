ebfb497b41d29d549cfd76fa654f34b391fb74b5
brian m. carlson <sandals@crustytoothpaste.net>
2011-05-09 22:10:21 +0000

    sha512: remove large tests.
    NIST has published several testcases that involve many iterations that take a
    long time.  While this is not a problem on some systems (3 minutes on a 2.0GHz
    Core 2 Duo), on older systems this can be prohibitive (3 hours 15 minutes on a
    333MHz Ultra 5).  Since these tests have been moved to the external testing
    routines (and were in fact the reason for creating the external testing
    routines), this does not cause any risk of incorrect code.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- impl/hash/sha512/sha512.cc
+++ impl/hash/sha512/sha512.cc
@@ -147,16 +147,6 @@ static int sha512test(void *, const drew_loader_t *)
 	res <<= 1;
 	res |= !HashTestCase<SHA512>("A", 1000).Test("329c52ac62d1fe731151f2b895a00475445ef74f50b979c6f7bb7cae349328c1d4cb4f7261a0ab43f936a24b000651d4a824fcdd577f211aef8f806b16afe8af");
 	res <<= 1;
-	res |= !HashTestCase<SHA512>("U", 1005).Test("59f5e54fe299c6a8764c6b199e44924a37f59e2b56c3ebad939b7289210dc8e4c21b9720165b0f4d4374c90f1bf4fb4a5ace17a1161798015052893a48c3d161");
-	res <<= 1;
-	res |= !HashTestCase<SHA512>(zero, 1, 1000000).Test("ce044bc9fd43269d5bbc946cbebc3bb711341115cc4abdf2edbc3ff2c57ad4b15deb699bda257fea5aef9c6e55fcf4cf9dc25a8c3ce25f2efe90908379bff7ed");
-	res <<= 1;
-	res |= !HashTestCase<SHA512>("Z", 0x20000000).Test("da172279f3ebbda95f6b6e1e5f0ebec682c25d3d93561a1624c2fa9009d64c7e9923f3b46bcaf11d39a531f43297992ba4155c7e827bd0f1e194ae7ed6de4cac");
-	res <<= 1;
-	res |= !HashTestCase<SHA512>(zero, 1, 0x41000000).Test("14b1be901cb43549b4d831e61e5f9df1c791c85b50e85f9d6bc64135804ad43ce8402750edbe4e5c0fc170b99cf78b9f4ecb9c7e02a157911d1bd1832d76784f");
-	res <<= 1;
-	res |= !HashTestCase<SHA512>("B", 0x6000003e).Test("fd05e13eb771f05190bd97d62647157ea8f1f6949a52bb6daaedbad5f578ec59b1b8d6c4a7ecb2feca6892b4dc138771670a0f3bd577eea326aed40ab7dd58b1");
-	res <<= 1;
 	res |= !HashTestCase<SHA512>::MaintenanceTest("2b237afa2664b8f340cdcd819861b477d55ca41b3538a12e961bb3eb5365f1078a88d993cf38ec080fcae5f43024660fb25264befbb79a2b1036e34908ba170c");
 
 	return res;
@@ -200,16 +190,6 @@ static int sha384test(void *, const drew_loader_t *)
 	res |= !HashTestCase<SHA384>(zero, 1, 1000).Test("aae017d4ae5b6346dd60a19d52130fb55194b6327dd40b89c11efc8222292de81e1a23c9b59f9f58b7f6ad463fa108ca");
 	res <<= 1;
 	res |= !HashTestCase<SHA384>("A", 1000).Test("7df01148677b7f18617eee3a23104f0eed6bb8c90a6046f715c9445ff43c30d69e9e7082de39c3452fd1d3afd9ba0689");
-	res <<= 1;
-	res |= !HashTestCase<SHA384>("U", 1005).Test("1bb8e256da4a0d1e87453528254f223b4cb7e49c4420dbfa766bba4adba44eeca392ff6a9f565bc347158cc970ce44ec");
-	res <<= 1;
-	res |= !HashTestCase<SHA384>(zero, 1, 1000000).Test("8a1979f9049b3fff15ea3a43a4cf84c634fd14acad1c333fecb72c588b68868b66a994386dc0cd1687b9ee2e34983b81");
-	res <<= 1;
-	res |= !HashTestCase<SHA384>("Z", 0x20000000).Test("18aded227cc6b562cc7fb259e8f404549e52914531aa1c5d85167897c779cc4b25d0425fd1590e40bd763ec3f4311c1a");
-	res <<= 1;
-	res |= !HashTestCase<SHA384>(zero, 1, 0x41000000).Test("83ab05ca483abe3faa597ad524d31291ae827c5be2b3efcb6391bfed31ccd937b6135e0378c6c7f598857a7c516f207a");
-	res <<= 1;
-	res |= !HashTestCase<SHA384>("B", 0x6000003e).Test("cf852304f8d80209351b37ce69ca7dcf34972b4edb7817028ec55ab67ad3bc96eecb8241734258a85d2afce65d4571e2");
 	res <<= 1;
 	res |= !HashTestCase<SHA384>::MaintenanceTest("03a953c09e78a5e0de89c9767037c56e86f2ba5575375355ac8607214452dadc710bacf3f50ec40a0de85cba01755b41");
 
