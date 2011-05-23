07206d0770395e59a61cf58ee1d6636c87ec6632
brian m. carlson <sandals@crustytoothpaste.net>
2011-05-23 23:54:54 +0000

    tls: add some useful data structures.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

---
+++ lib/libdrew-tls/structures.cc
@@ -0,0 +1,158 @@
+#include "structures.hh"
+#include "util.hh"
+
+SerializedBuffer::SerializedBuffer()
+{
+	Reset();
+}
+
+SerializedBuffer::SerializedBuffer(size_t sz)
+{
+	Reset(sz);
+}
+
+SerializedBuffer::~SerializedBuffer()
+{
+	memset(buf, 0, buflen);
+	free(buf);
+}
+
+SerializedBuffer::SerializedBuffer(const SerializedBuffer &b)
+{
+	memset(buf, 0, buflen);
+	free(buf);
+
+	off = b.off;
+	len = b.len;
+	buflen = b.buflen;
+	posix_memalign((void **)&buf, 16, buflen);
+	memcpy(buf, b.buf, buflen);
+}
+
+void SerializedBuffer::ResetPosition()
+{
+	off = 0;
+}
+
+void SerializedBuffer::Reset()
+{
+	Reset(512);
+}
+
+void SerializedBuffer::Reset(size_t sz)
+{
+	size_t space = (sz + 0x1ff) & ~0x1ff;
+	posix_memalign((void **)&buf, 16, space);
+	off = 0;
+	len = 0;
+	buflen = space;
+}
+
+void SerializedBuffer::Put(uint8_t x)
+{
+	Put(&x, 1);
+}
+
+template<class T>
+void SerializedBuffer::Put(T x)
+{
+	uint8_t buf[sizeof(T)];
+	BigEndian::Copy(buf, &x, sizeof(T));
+	Put(buf, sizeof(T));
+}
+
+void SerializedBuffer::Put(const uint8_t *data, size_t datalen)
+{
+	ExtendIfNecessary(datalen);
+	memcpy(buf+off, data, datalen);
+	off += datalen;
+	len += datalen;
+}
+
+void SerializedBuffer::Put(SerializedBuffer &sbuf)
+{
+	Put(sbuf.buf, sbuf.len);
+}
+
+void SerializedBuffer::Get(uint8_t &x)
+{
+	Get(&x, 1);
+}
+
+template<class T>
+void SerializedBuffer::Get(T &x)
+{
+	uint8_t buf[sizeof(T)];
+	Get(buf, sizeof(T));
+	BigEndian::Copy(&x, buf, sizeof(T));
+}
+
+void SerializedBuffer::Get(uint8_t *data, size_t datalen)
+{
+	memcpy(data, buf+off, datalen);
+	off += datalen;
+}
+
+void SerializedBuffer::Get(SerializedBuffer &sbuf, size_t datalen)
+{
+	uint8_t *data = new uint8_t[datalen];
+
+	memcpy(data, buf+off, datalen);
+	off += datalen;
+	sbuf.Put(data, datalen);
+	memset(data, 0, datalen);
+	delete[] data;
+}
+
+void SerializedBuffer::Reserve(size_t space)
+{
+	if (space > buflen) {
+		size_t extra = (space - buflen + 0x1ff) & ~0x1ff;
+		uint8_t *tmp;
+		posix_memalign((void **)&tmp, 16, buflen + extra);
+		memcpy(tmp, buf, buflen);
+		memset(buf, 0, buflen);
+		free(buf);
+		buflen += extra;
+	}
+}
+
+void SerializedBuffer::ExtendIfNecessary(size_t space)
+{
+	Reserve(off + space);
+}
+
+size_t SerializedBuffer::GetLength() const
+{
+	return len;
+}
+
+ssize_t SerializedBuffer::BytesRemaining() const
+{
+	return ssize_t(len) - ssize_t(off);
+}
+
+const uint8_t *SerializedBuffer::GetPointer() const
+{
+	return buf + off;
+}
+
+const uint8_t *SerializedBuffer::GetPointer(size_t offset) const
+{
+	return buf + offset;
+}
+
+uint8_t *SerializedBuffer::GetPointer()
+{
+	return buf + off;
+}
+
+uint8_t *SerializedBuffer::GetPointer(size_t offset)
+{
+	return buf + offset;
+}
+
+size_t SerializedBuffer::GetOffset() const
+{
+	return off;
+}
---
+++ lib/libdrew-tls/structures.hh
@@ -0,0 +1,126 @@
+#ifndef DREW_TLS_STRUCTURES_HH
+#define DREW_TLS_STRUCTURES_HH
+
+#include "internal.h"
+#include "util.hh"
+
+#include <stdint.h>
+#include <pthread.h>
+#include <sys/types.h>
+
+#include <drew-tls/drew-tls.h>
+
+// None of these classes are thread-safe.
+
+class MutexLock
+{
+	public:
+		MutexLock(pthread_mutex_t *m)
+		{
+			mutex = m;
+			pthread_mutex_lock(mutex);
+		}
+		~MutexLock()
+		{
+			pthread_mutex_unlock(mutex);
+		}
+	private:
+		pthread_mutex_t *mutex;
+};
+
+class SerializedBuffer
+{
+	public:
+		SerializedBuffer();
+		SerializedBuffer(size_t sz);
+		SerializedBuffer(const SerializedBuffer &b);
+		~SerializedBuffer();
+		void ResetPosition();
+		void Reset();
+		void Put(uint8_t x);
+		void Put(const uint8_t *data, size_t datalen);
+		void Put(SerializedBuffer &sbuf);
+		template<class T>
+		void Put(T x);
+		void Get(uint8_t &x);
+		template<class T>
+		void Get(T &x);
+		void Get(uint8_t *data, size_t datalen);
+		void Get(SerializedBuffer &sbuf, size_t datalen);
+		size_t GetLength() const;
+		size_t GetOffset() const;
+		ssize_t BytesRemaining() const;
+		const uint8_t *GetPointer() const;
+		const uint8_t *GetPointer(size_t offset) const;
+		uint8_t *GetPointer();
+		uint8_t *GetPointer(size_t offset);
+		void Reserve(size_t);
+	protected:
+		void Reset(size_t);
+		void ExtendIfNecessary(size_t space);
+	private:
+		uint8_t *buf;
+		size_t len;		// The length of the actual data.
+		size_t off;
+		size_t buflen;	// The length of buf.
+};
+
+struct ProtocolVersion
+{
+	uint8_t major;
+	uint8_t minor;
+	int Compare(const ProtocolVersion &x)
+	{
+		if (this->major < x.major)
+			return -1;
+		if (this->major > x.major)
+			return 1;
+		if (this->minor < x.minor)
+			return -1;
+		if (this->minor > x.minor)
+			return 1;
+		return 0;
+	}
+	int ReadFromBuffer(SerializedBuffer &buf)
+	{
+		if (buf.BytesRemaining() < 2)
+			return -DREW_TLS_ERR_RECORD_OVERFLOW;
+		buf.Get(major);
+		buf.Get(minor);
+		return 0;
+	}
+	void WriteToBuffer(SerializedBuffer &buf)
+	{
+		buf.Put(major);
+		buf.Put(minor);
+	}
+};
+
+struct Record
+{
+	uint8_t type; // ContentType
+	ProtocolVersion version;
+	uint16_t length;
+	SerializedBuffer data;
+
+	int ReadFromBuffer(SerializedBuffer &buf)
+	{
+		if (buf.BytesRemaining() < 5)
+			return -DREW_TLS_ERR_RECORD_OVERFLOW;
+		buf.Get(type);
+		version.ReadFromBuffer(buf);
+		buf.Get(length);
+		if (buf.BytesRemaining() < length)
+			return -DREW_TLS_ERR_RECORD_OVERFLOW;
+		buf.Get(data, length);
+	}
+	void WriteToBuffer(SerializedBuffer &buf)
+	{
+		buf.Put(type);
+		version.WriteToBuffer(buf);
+		buf.Put(length);
+		buf.Put(data);
+	}
+};
+
+#endif
