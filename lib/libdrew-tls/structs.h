63837a7cd8625c2c4baa7ddbbc1c76ecd7200b2e
brian m. carlson <sandals@crustytoothpaste.net>
2011-05-24 21:28:58 +0000

    tls: move session definition to internal header.
    The session struct definition does not need to be present in the public header
    since nobody is going to be allocating one outside of the session code.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- lib/libdrew-tls/structs.h
+++ lib/libdrew-tls/structs.h
@@ -115,4 +115,27 @@ typedef struct {
 	drew_tls_compression_method_t compression_method;
 } drew_tls_server_hello_t;
 
+struct drew_tls_session_s {
+	int client; // is this the client end or the server end?
+	int enc_type;
+	const drew_loader_t *ldr;
+	drew_prng_t *prng;
+	uint8_t block_size;
+	uint8_t hash_size;
+	drew_mac_t *inmac;
+	drew_mode_t *inmode;
+	uint64_t inseqnum;
+	drew_mac_t *outmac;
+	drew_mode_t *outmode;
+	uint64_t outseqnum;
+	drew_tls_priority_t prio;
+	drew_tls_session_id_t session_id;
+	drew_tls_protocol_version_t protover;
+	drew_tls_data_ctxt_t data_inp;
+	drew_tls_data_ctxt_t data_outp;
+	drew_tls_data_in_func_t data_infunc;
+	drew_tls_data_out_func_t data_outfunc;
+	DREW_TLS_MUTEX_DECL()
+};
+
 #endif
