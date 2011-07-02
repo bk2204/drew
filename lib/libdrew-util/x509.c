b570ebb19ba44fb112f18f6ddfb94fee7c109f85
brian m. carlson <sandals@crustytoothpaste.net>
2011-07-02 02:48:15 +0000

    x509: fix misparse of certificate version number.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- lib/libdrew-util/x509.c
+++ lib/libdrew-util/x509.c
@@ -12,8 +12,12 @@ int drew_util_asn1_x509_parse_version(drew_util_asn1_t asn,
 {
 	int res = 0;
 	ssize_t ver = 0;
-	if ((res = drew_util_asn1_parse_small_integer(asn, val, &ver)))
+	drew_util_asn1_value_t tmp;
+	if (val->tagclass != DREW_UTIL_ASN1_TC_CONTEXT || val->tag != 0)
+		return -DREW_ERR_INVALID;
+	if ((res = drew_util_asn1_parse(asn, val->data, val->length, &tmp)) < 0)
 		return res;
+	RETFAIL(drew_util_asn1_parse_small_integer(asn, &tmp, &ver));
 	if (ver < 0)
 		return -DREW_ERR_INVALID;
 	*version = ver + 1;
