6bb558ae7ac6f4169edec984b18f0987704bb927
brian m. carlson <sandals@crustytoothpaste.net>
2011-07-02 02:06:59 +0000

    drew-util: add basic X.509 facilities.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

---
+++ include/drew-util/x509.h
@@ -0,0 +1,26 @@
+#ifndef DREW_UTIL_X509_H
+#define DREW_UTIL_X509_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include <drew-util/asn1.h>
+
+typedef struct drew_util_x509_sig_s {
+	drew_util_asn1_oid_t algo;
+	uint8_t *data;
+	size_t nbits;
+} drew_util_x509_sig_t;
+
+typedef struct drew_util_x509_cert_s {
+	int version;
+	drew_util_x509_sig_t sig;
+	time_t not_before;
+	time_t not_after;
+} drew_util_x509_cert_t;
+
+int drew_util_x509_parse_certificate(drew_util_asn1_t asn,
+		const uint8_t *data, size_t len, drew_util_x509_cert_t *cert);
+
+#endif
