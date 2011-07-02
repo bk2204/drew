73af83715b7d76aa8ea2987f9b9a95a4bc466d8e
brian m. carlson <sandals@crustytoothpaste.net>
2011-07-02 02:06:59 +0000

    drew-util: add basic X.509 facilities.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

---
+++ lib/libdrew-util/x509.c
@@ -0,0 +1,56 @@
+#include "internal.h"
+
+#include <errno.h>
+#include <stdlib.h>
+
+#include <drew/drew.h>
+#include <drew-util/asn1.h>
+#include <drew-util/x509.h>
+
+int drew_util_asn1_x509_parse_version(drew_util_asn1_t asn,
+		const drew_util_asn1_value_t *val, int *version)
+{
+	int res = 0;
+	ssize_t ver = 0;
+	if ((res = drew_util_asn1_parse_small_integer(asn, val, &ver)))
+		return res;
+	if (ver < 0)
+		return -DREW_ERR_INVALID;
+	*version = ver + 1;
+	return 0;
+}
+
+int drew_util_x509_parse_certificate(drew_util_asn1_t asn,
+		const uint8_t *data, size_t len, drew_util_x509_cert_t *cert)
+{
+	int res = 0;
+	uint8_t *p;
+	size_t ncertvals, nvals, nsigvals;
+	drew_util_asn1_value_t certificate, *certvals, *sigvals, *vals;
+
+	// FIXME: remove memory leaks.
+	res = drew_util_asn1_parse(asn, data, len, &certificate);
+	if (res < 0)
+		return res;
+
+	RETFAIL(drew_util_asn1_parse_sequence(asn, &certificate, &certvals,
+				&ncertvals));
+	if (ncertvals != 3)
+		return -DREW_ERR_INVALID;
+
+	// FIXME: parse the entire certificate.
+	RETFAIL(drew_util_asn1_parse_sequence(asn, &certvals[0], &vals, &nvals));
+	RETFAIL(drew_util_asn1_x509_parse_version(asn, &vals[0], &cert->version));
+
+	RETFAIL(drew_util_asn1_parse_sequence(asn, &certvals[1], &sigvals,
+				&nsigvals));
+	RETFAIL(drew_util_asn1_parse_oid(asn, sigvals, &cert->sig.algo));
+
+	if (!(p = malloc(certvals[2].length)))
+		return -ENOMEM;
+	cert->sig.data = p;
+	RETFAIL(drew_util_asn1_parse_bitstring(asn, &certvals[2], p,
+				&cert->sig.nbits));
+
+	return res;
+}
