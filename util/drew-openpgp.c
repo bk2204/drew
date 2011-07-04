9aba844f51e0be5df93865c6af0a19c59ac02eb7
brian m. carlson <sandals@crustytoothpaste.net>
2011-07-04 23:39:08 +0000

    drew-openpgp: implement fingerprint generation.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- util/drew-openpgp.c
+++ util/drew-openpgp.c
@@ -7,8 +7,14 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 
+#include <drew/drew.h>
+#include <drew/plugin.h>
+
 #include <drew-opgp/drew-opgp.h>
+#include <drew-opgp/key.h>
 #include <drew-opgp/parser.h>
+
+#define DIM(x) (sizeof(x)/sizeof(x[0]))
 
 const char *programname;
 
@@ -80,6 +86,66 @@ out:
 	return res;
 }
 
+int print_fingerprint(const char *filename)
+{
+	uint8_t *buf;
+	int fd, res = 0;
+	struct stat st;
+	size_t off = 0;
+	drew_opgp_parser_t pars;
+	// FIXME: do not hardcode this.
+	drew_opgp_packet_t pkts[50];
+	drew_opgp_key_t key;
+	drew_opgp_fp_t fp;
+	drew_opgp_id_t id;
+	size_t npkts = DIM(pkts);
+	drew_loader_t *ldr;
+
+	if ((fd = open(filename, O_RDONLY)) < 0)
+		return print_error(16, errno, "couldn't open %s", filename);
+	if (fstat(fd, &st))
+		return print_error(17, errno, "couldn't fstat %s", filename);
+	buf = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+	if (buf == MAP_FAILED)
+		return print_error(18, errno, "couldn't mmap %s", filename);
+	drew_loader_new(&ldr);
+	drew_loader_load_plugin(ldr, NULL, NULL);
+	drew_loader_load_plugin(ldr, "sha1", "./plugins");
+	drew_loader_load_plugin(ldr, "sha256", "./plugins");
+	drew_opgp_parser_new(&pars, 0, NULL);
+	res = drew_opgp_parser_parse_packets(pars, pkts, &npkts, buf, st.st_size,
+			&off);
+	if (res < 0) {
+		res = print_error(19, res, "failed parsing packets");
+		goto out;
+	}
+	drew_opgp_key_new(&key, ldr);
+	if ((res = drew_opgp_key_load_public(key, pkts, npkts) < 0)) {
+		res = print_error(20, res, "failed loading packets");
+		goto out;
+	}
+	if ((res = drew_opgp_key_synchronize(key, 0)) < 0) {
+		res = print_error(21, res, "failed to synchronize");
+		goto out;
+	}
+	drew_opgp_key_get_fingerprint(key, fp);
+	drew_opgp_key_get_id(key, id);
+	drew_opgp_key_free(&key);
+	printf("fingerprint: ");
+	for (size_t i = 0; i < 20; i++)
+		printf("%02x", fp[i]);
+	printf("\n");
+	printf("id: ");
+	for (size_t i = 0; i < 32; i++)
+		printf("%02x", id[i]);
+	printf("\n");
+	res = 0;
+out:
+	drew_opgp_parser_free(&pars);
+	drew_loader_free(&ldr);
+	return res;
+}
+
 int main(int argc, char **argv)
 {
 	programname = argv[0];
@@ -89,6 +155,9 @@ int main(int argc, char **argv)
 	}
 	if (!strcmp(argv[1], "--list-packets")) {
 		return list_packets(argv[2]);
+	}
+	else if (!strcmp(argv[1], "--fingerprint")) {
+		return print_fingerprint(argv[2]);
 	}
 	else {
 		fprintf(stderr, "%s: only --list-packets is supported\n", argv[0]);
