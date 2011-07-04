00175522d2bd2d7b3ecdb78e9e830b82313a074d
brian m. carlson <sandals@crustytoothpaste.net>
2011-07-04 23:24:54 +0000

    openpgp: define constants for digests and ciphers.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- include/drew-opgp/drew-opgp.h
+++ include/drew-opgp/drew-opgp.h
@@ -14,4 +14,31 @@
 // The data is not in compliance with the versions specified.
 #define DREW_OPGP_ERR_WRONG_VERSION		0x20101
 
+#define DREW_OPGP_MDALGO_MD5			1
+#define DREW_OPGP_MDALGO_SHA1			2
+#define DREW_OPGP_MDALGO_RIPEMD160		3
+// Algorithm 4 was reserved for double-width SHA, which was never defined.
+#define DREW_OPGP_MDALGO_MD2			5
+#define DREW_OPGP_MDALGO_TIGER192		6
+#define DREW_OPGP_MDALGO_HAVAL5160		7
+#define DREW_OPGP_MDALGO_SHA256			8
+#define DREW_OPGP_MDALGO_SHA384			9
+#define DREW_OPGP_MDALGO_SHA512			10
+#define DREW_OPGP_MDALGO_SHA224			11
+
+#define DREW_OPGP_SKALGO_NONE			0
+#define DREW_OPGP_SKALGO_IDEA			1
+#define DREW_OPGP_SKALGO_3DES			2
+#define DREW_OPGP_SKALGO_CAST5			3
+#define DREW_OPGP_SKALGO_BLOWFISH		4
+#define DREW_OPGP_SKALGO_SAFERSK128		5
+// Algorithm 6 was reserved for DES/SK.
+#define DREW_OPGP_SKALGO_AES128			7
+#define DREW_OPGP_SKALGO_AES192			8
+#define DREW_OPGP_SKALGO_AES256			9
+#define DREW_OPGP_SKALGO_TWOFISH		10
+#define DREW_OPGP_SKALGO_CAMELLIA128	11
+#define DREW_OPGP_SKALGO_CAMELLIA192	12
+#define DREW_OPGP_SKALGO_CAMELLIA256	13
+
 #endif
