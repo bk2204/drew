9dec3b25b58e34a91405ca330a6c32f8dbcf9fb7
brian m. carlson <sandals@crustytoothpaste.net>
2011-07-05 00:49:22 +0000

    drew-openpgp: refactor.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- util/drew-openpgp.c
+++ util/drew-openpgp.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 #include <drew/drew.h>
 #include <drew/plugin.h>
@@ -53,26 +54,85 @@ void print_packet(const drew_opgp_packet_t *pkt)
 	}
 }
 
-int list_packets(const char *filename)
-{
+struct file {
 	uint8_t *buf;
-	int fd, res = 0;
-	struct stat st;
-	off_t off = 0;
+	int fd;
+	off_t size;
+	const char *filename;
+};
+
+struct util {
+	drew_loader_t *ldr;
 	drew_opgp_parser_t pars;
+};
+
+int open_file(struct file *f, const char *filename)
+{
+	struct stat st;
+	if ((f->fd = open(filename, O_RDONLY)) < 0)
+		return print_error(16, errno, "couldn't open %s", filename);
+	if (fstat(f->fd, &st))
+		return print_error(17, errno, "couldn't fstat %s", filename);
+	f->size = st.st_size;
+	f->buf = mmap(NULL, f->size, PROT_READ, MAP_PRIVATE, f->fd, 0);
+	if (f->buf == MAP_FAILED)
+		return print_error(18, errno, "couldn't mmap %s", filename);
+	f->filename = filename;
+	return 0;
+}
+
+void close_file(struct file *f)
+{
+	munmap(f->buf, f->size);
+	close(f->fd);
+}
+
+int create_util(struct util *util)
+{
+	int res = 0;
+	const char *pluginnames[] = {
+		"md5",
+		"md2",
+		"sha1",
+		"sha256",
+		"sha512",
+		"ripe160",
+		"blowfish",
+		"aes",
+		"camellia",
+		"des",
+		"cast5"
+	};
+
+	if ((res = drew_loader_new(&util->ldr)))
+		return print_error(5, res, "couldn't create loader");
+	drew_loader_load_plugin(util->ldr, NULL, NULL);
+	for (size_t i = 0; i < DIM(pluginnames); i++) {
+		drew_loader_load_plugin(util->ldr, pluginnames[i], NULL);
+		drew_loader_load_plugin(util->ldr, pluginnames[i], "./plugins");
+	}
+	if ((res = drew_opgp_parser_new(&util->pars, 0, NULL))) {
+		drew_loader_free(&util->ldr);
+		return print_error(6, res, "couldn't create packet parser");
+	}
+	return 0;
+}
+
+void destroy_util(struct util *util)
+{
+	drew_opgp_parser_free(&util->pars);
+	drew_loader_free(&util->ldr);
+}
+
+int list_packets(struct file *f, struct util *util)
+{
+	int res = 0;
+	off_t off = 0;
 	drew_opgp_packet_t pkt;
 
-	if ((fd = open(filename, O_RDONLY)) < 0)
-		return print_error(16, errno, "couldn't open %s", filename);
-	if (fstat(fd, &st))
-		return print_error(17, errno, "couldn't fstat %s", filename);
-	buf = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-	if (buf == MAP_FAILED)
-		return print_error(18, errno, "couldn't mmap %s", filename);
-	drew_opgp_parser_new(&pars, 0, NULL);
-	while (res >= 0 && off < st.st_size) {
-		res = drew_opgp_parser_parse_packet(pars, &pkt, buf+off,
-				st.st_size-off);
+	while (res >= 0 && off < f->size) {
+		res = drew_opgp_parser_parse_packet(util->pars, &pkt, f->buf+off,
+				f->size-off);
 		if (res < 0) {
 			res = print_error(19, res, "failed parsing packet");
 			goto out;
@@ -82,17 +142,13 @@ int list_packets(const char *filename)
 	}
 	res = 0;
 out:
-	drew_opgp_parser_free(&pars);
 	return res;
 }
 
-int print_fingerprint(const char *filename)
+int print_fingerprint(struct file *f, struct util *util)
 {
-	uint8_t *buf;
-	int fd, res = 0;
-	struct stat st;
+	int res = 0;
 	size_t off = 0;
-	drew_opgp_parser_t pars;
 	// FIXME: do not hardcode this.
 	drew_opgp_packet_t pkts[50];
 	drew_opgp_key_t key;
@@ -101,27 +157,14 @@ int print_fingerprint(const char *filename)
 	drew_opgp_keyid_t keyid;
 	int version;
 	size_t npkts = DIM(pkts);
-	drew_loader_t *ldr;
 
-	if ((fd = open(filename, O_RDONLY)) < 0)
-		return print_error(16, errno, "couldn't open %s", filename);
-	if (fstat(fd, &st))
-		return print_error(17, errno, "couldn't fstat %s", filename);
-	buf = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-	if (buf == MAP_FAILED)
-		return print_error(18, errno, "couldn't mmap %s", filename);
-	drew_loader_new(&ldr);
-	drew_loader_load_plugin(ldr, NULL, NULL);
-	drew_loader_load_plugin(ldr, "sha1", "./plugins");
-	drew_loader_load_plugin(ldr, "sha256", "./plugins");
-	drew_opgp_parser_new(&pars, 0, NULL);
-	res = drew_opgp_parser_parse_packets(pars, pkts, &npkts, buf, st.st_size,
-			&off);
+	res = drew_opgp_parser_parse_packets(util->pars, pkts, &npkts, f->buf,
+			f->size, &off);
 	if (res < 0) {
 		res = print_error(19, res, "failed parsing packets");
 		goto out;
 	}
-	drew_opgp_key_new(&key, ldr);
+	drew_opgp_key_new(&key, util->ldr);
 	if ((res = drew_opgp_key_load_public(key, pkts, npkts) < 0)) {
 		res = print_error(20, res, "failed loading packets");
 		goto out;
@@ -149,23 +192,42 @@ int print_fingerprint(const char *filename)
 	printf("\n");
 	res = 0;
 out:
-	drew_opgp_parser_free(&pars);
-	drew_loader_free(&ldr);
 	return res;
 }
 
 int main(int argc, char **argv)
 {
+	int res = 0;
+	struct file f;
+	struct util util;
 	programname = argv[0];
 	if (argc < 3) {
 		fprintf(stderr, "%s: need at least two arguments\n", argv[0]);
 		return 2;
 	}
 	if (!strcmp(argv[1], "--list-packets")) {
-		return list_packets(argv[2]);
+		if ((res = open_file(&f, argv[2])))
+			return res;
+		if ((res = create_util(&util))) {
+			close_file(&f);
+			return res;
+		}
+		res = list_packets(&f, &util);
+		destroy_util(&util);
+		close_file(&f);
+		return res;
 	}
 	else if (!strcmp(argv[1], "--fingerprint")) {
-		return print_fingerprint(argv[2]);
+		if ((res = open_file(&f, argv[2])))
+			return res;
+		if ((res = create_util(&util))) {
+			close_file(&f);
+			return res;
+		}
+		res = print_fingerprint(&f, &util);
+		destroy_util(&util);
+		close_file(&f);
+		return res;
 	}
 	else {
 		fprintf(stderr, "%s: only --list-packets is supported\n", argv[0]);
