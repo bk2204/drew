9e65708c59debc998fd0cd663c73d524ac6816cb
brian m. carlson <sandals@crustytoothpaste.net>
2011-07-07 22:20:02 +0000

    openpgp: split out structures into a header.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- lib/libdrew-openpgp/key.c
+++ lib/libdrew-openpgp/key.c
@@ -1,4 +1,5 @@
 #include "internal.h"
+#include "structs.h"
 
 #include <arpa/inet.h>
 #include <errno.h>
@@ -14,78 +15,6 @@
 #include <drew-opgp/drew-opgp.h>
 #include <drew-opgp/key.h>
 #include <drew-opgp/parser.h>
-
-struct drew_opgp_signature_s {
-	int flags;
-	uint8_t ver;
-	uint8_t type;
-	uint8_t pkalgo;
-	uint8_t mdalgo;
-	time_t ctime;
-	drew_opgp_keyid_t keyid;
-	size_t hashedlen;	
-	size_t nhashed;
-	uint8_t *hasheddata;
-	drew_opgp_subpacket_t *hashed;
-	size_t unhashedlen;	
-	size_t nunhashed;
-	uint8_t *unhasheddata;
-	drew_opgp_subpacket_t *unhashed;
-	uint8_t left[2];
-	drew_opgp_mpi_t mpi[DREW_OPGP_MAX_MPIS];
-	drew_opgp_hash_t hash;
-	drew_opgp_id_t id;
-};
-
-typedef struct drew_opgp_signature_s csig_t;
-typedef struct drew_opgp_key_s ckey_t;
-
-typedef struct {
-	char *s;
-	size_t len;
-	csig_t *theselfsig;
-	csig_t *selfsigs;
-	size_t nselfsigs;
-	csig_t *sigs;
-	size_t nsigs;
-	drew_opgp_id_t id;
-} cuid_t;
-
-typedef struct drew_opgp_pubkey_s {
-	int state;
-	uint8_t ver;
-	uint8_t algo;
-	time_t ctime;
-	time_t etime;
-	struct drew_opgp_pubkey_s *parent;
-	drew_opgp_mpi_t mpi[DREW_OPGP_MAX_MPIS];
-	cuid_t *uids;
-	size_t nuids;
-	csig_t *sigs;
-	size_t nsigs;
-	drew_opgp_keyid_t keyid;
-	drew_opgp_id_t id;
-	drew_opgp_fp_t fp;
-} pubkey_t;
-
-typedef struct drew_opgp_privkey_s {
-	int state;
-	struct drew_opgp_privkey_s *parent;
-	pubkey_t *pub;
-	drew_opgp_mpi_t mpi[DREW_OPGP_MAX_MPIS];
-	drew_opgp_id_t id;
-} privkey_t;
-
-struct drew_opgp_key_s {
-	pubkey_t pub;
-	pubkey_t *pubsubs;
-	size_t npubsubs;
-	privkey_t priv;
-	privkey_t *privsubs;
-	size_t nprivsubs;
-	drew_opgp_id_t id;
-	const drew_loader_t *ldr;
-};
 
 int drew_opgp_key_new(drew_opgp_key_t *key, const drew_loader_t *ldr)
 {
