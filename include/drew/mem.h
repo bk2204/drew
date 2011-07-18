02ce99fce9dc2f47ee807d4da405561c78a62f17
brian m. carlson <sandals@crustytoothpaste.net>
2011-07-18 22:43:14 +0000

    Mark memory functions as extern "C".
    We should do this anyway, but if we don't, we get undefined symbol errors when
    trying to link C++ code that uses the memory functions.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- include/drew/mem.h
+++ include/drew/mem.h
@@ -1,6 +1,12 @@
 #ifndef DREW_MEM_H
 #define DREW_MEM_H
 
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void *drew_mem_malloc(size_t size);
 void *drew_mem_calloc(size_t nmemb, size_t size);
 void *drew_mem_realloc(void *ptr, size_t size);
@@ -12,5 +18,9 @@ void *drew_mem_scalloc(size_t nmemb, size_t size);
 void *drew_mem_srealloc(void *ptr, size_t size);
 void *drew_mem_smemdup(const void *ptr, size_t size);
 void drew_mem_sfree(void *ptr);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
