5827d6f9f7b8cce570dcccf8d6cdb7c8aacf6a30
brian m. carlson <sandals@crustytoothpaste.net>
2011-07-19 19:08:53 +0000

    Implement PRF functionality for HMAC.
    Since HMAC can be used as a PRF, implement the interface required to do that.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- impl/mac/hmac/hmac.c
+++ impl/mac/hmac/hmac.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <drew/kdf.h>
 #include <drew/mac.h>
 #include <drew/mem.h>
 #include <drew/hash.h>
@@ -19,6 +20,11 @@
  * well as the digest size.
  */
 #define BUFFER_SIZE		256
+
+/* Interfaces must not modify the functbl member of the context since the KDF
+ * implementation casts its context to the MAC implementation to avoid
+ * duplicating lots of code needlessly.
+ */
 
 struct hmac {
 	const drew_loader_t *ldr;
@@ -33,7 +39,7 @@ struct hmac {
 
 static int hmac_info(int op, void *p)
 {
-	return -EINVAL;
+	return -DREW_ERR_NOT_IMPL;
 }
 
 static int hmac_init(drew_mac_t *ctx, int flags, const drew_loader_t *ldr,
@@ -51,6 +57,8 @@ static int hmac_init(drew_mac_t *ctx, int flags, const drew_loader_t *ldr,
 		return -EINVAL;
 
 	struct hmac *p = drew_mem_smalloc(sizeof(*p));
+	if (!p)
+		return -ENOMEM;
 	memset(p, 0, sizeof(*p));
 	p->ldr = ldr;
 	p->outside.functbl = p->inside.functbl = algo->functbl;
@@ -94,7 +102,7 @@ static int hmac_fini(drew_mac_t *ctx, int flags)
 	struct hmac *h = ctx->ctx;
 	h->outside.functbl->fini(&h->outside, 0);
 	h->inside.functbl->fini(&h->inside, 0);
-	memset(h, 0, sizeof(*h));
+	drew_mem_sfree(h);
 
 	if (!(flags & DREW_MAC_FIXED))
 		ctx->ctx = NULL;
@@ -228,98 +236,98 @@ static int hmac_test_generic(const drew_loader_t *ldr, const char *name,
 }
 
 #define U8P (const uint8_t *)
+#define EIGHTY_AA U8P "\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa" \
+	"\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa" \
+	"\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa" \
+	"\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa" \
+	"\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa" \
+	"\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa" \
+	"\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa" \
+	"\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa" \
+	"\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa" \
+	"\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa"
+static const struct test testdata_md5[] = {
+	{
+		U8P "\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b"
+			"\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b",
+		16,
+		U8P "Hi There",
+		8,
+		1,
+		U8P "\x92\x94\x72\x7a\x36\x38\xbb\x1c"
+			"\x13\xf4\x8e\xf8\x15\x8b\xfc\x9d",
+	},
+	{
+		U8P "Jefe",
+		4,
+		U8P "what do ya want for nothing?",
+		28,
+		1,
+		U8P "\x75\x0c\x78\x3e\x6a\xb0\xb5\x03"
+			"\xea\xa8\x6e\x31\x0a\x5d\xb7\x38",
+	},
+	{
+		U8P	"\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa"
+			"\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa",
+		16,
+		U8P "\xdd",
+		1,
+		50,
+		U8P "\x56\xbe\x34\x52\x1d\x14\x4c\x88"
+			"\xdb\xb8\xc7\x33\xf0\xe8\xb3\xf6",
+	},
+	{
+		U8P "\x01\x02\x03\x04\x05\x06\x07\x08"
+			"\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10"
+			"\x11\x12\x13\x14\x15\x16\x17\x18"
+			"\x19",
+		25,
+		U8P "\xcd",
+		1,
+		50,
+		U8P	"\x69\x7e\xaf\x0a\xca\x3a\x3a\xea"
+			"\x3a\x75\x16\x47\x46\xff\xaa\x79",
+
+	},
+	{
+		U8P "\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c"
+			"\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c",
+		16,
+		U8P "Test With Truncation",
+		20,
+		1,
+		U8P	"\x56\x46\x1e\xf2\x34\x2e\xdc\x00"
+			"\xf9\xba\xb9\x95\x69\x0e\xfd\x4c",
+
+	},
+	{
+		EIGHTY_AA,
+		80,
+		U8P "Test Using Larger Than Block-Size Key - Hash Key First",
+		54,
+		1,
+		U8P "\x6b\x1a\xb7\xfe\x4b\xd7\xbf\x8f"
+			"\x0b\x62\xe6\xce\x61\xb9\xd0\xcd",
+
+	},
+	{
+		EIGHTY_AA,
+		80,
+		U8P "Test Using Larger Than Block-Size Key and Larger "
+			"Than One Block-Size Data",
+		73,
+		1,
+		U8P "\x6f\x63\x0f\xad\x67\xcd\xa0\xee"
+			"\x1f\xb1\xf5\x62\xdb\x3a\xa5\x3e"
+
+	}
+};
+
 static int hmac_test_md5(const drew_loader_t *ldr, size_t *ntests)
 {
-	const uint8_t *eighty_aa = U8P "\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa"
-		"\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa"
-		"\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa"
-		"\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa"
-		"\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa"
-		"\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa"
-		"\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa"
-		"\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa"
-		"\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa"
-		"\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa";
-	struct test testdata[] = {
-		{
-			U8P "\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b"
-				"\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b",
-			16,
-			U8P "Hi There",
-			8,
-			1,
-			U8P "\x92\x94\x72\x7a\x36\x38\xbb\x1c"
-				"\x13\xf4\x8e\xf8\x15\x8b\xfc\x9d",
-		},
-		{
-			U8P "Jefe",
-			4,
-			U8P "what do ya want for nothing?",
-			28,
-			1,
-			U8P "\x75\x0c\x78\x3e\x6a\xb0\xb5\x03"
-				"\xea\xa8\x6e\x31\x0a\x5d\xb7\x38",
-		},
-		{
-			U8P	"\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa"
-				"\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa",
-			16,
-			U8P "\xdd",
-			1,
-			50,
-			U8P "\x56\xbe\x34\x52\x1d\x14\x4c\x88"
-				"\xdb\xb8\xc7\x33\xf0\xe8\xb3\xf6",
-		},
-		{
-			U8P "\x01\x02\x03\x04\x05\x06\x07\x08"
-				"\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10"
-				"\x11\x12\x13\x14\x15\x16\x17\x18"
-				"\x19",
-			25,
-			U8P "\xcd",
-			1,
-			50,
-			U8P	"\x69\x7e\xaf\x0a\xca\x3a\x3a\xea"
-				"\x3a\x75\x16\x47\x46\xff\xaa\x79",
- 
-		},
-		{
-			U8P "\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c"
-				"\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c",
-			16,
-			U8P "Test With Truncation",
-			20,
-			1,
-			U8P	"\x56\x46\x1e\xf2\x34\x2e\xdc\x00"
-				"\xf9\xba\xb9\x95\x69\x0e\xfd\x4c",
- 
-		},
-		{
-			eighty_aa,
-			80,
-			U8P "Test Using Larger Than Block-Size Key - Hash Key First",
-			54,
-			1,
-			U8P "\x6b\x1a\xb7\xfe\x4b\xd7\xbf\x8f"
-				"\x0b\x62\xe6\xce\x61\xb9\xd0\xcd",
- 
-		},
-		{
-			eighty_aa,
-			80,
-			U8P "Test Using Larger Than Block-Size Key and Larger "
-				"Than One Block-Size Data",
-			73,
-			1,
-			U8P "\x6f\x63\x0f\xad\x67\xcd\xa0\xee"
-				"\x1f\xb1\xf5\x62\xdb\x3a\xa5\x3e"
- 
-		}
-	};
+	*ntests = DIM(testdata_md5);
 
-	*ntests = DIM(testdata);
-
-	return hmac_test_generic(ldr, "MD5", testdata, DIM(testdata), 16);
+	return hmac_test_generic(ldr, "MD5", testdata_md5, DIM(testdata_md5), 16);
 }
 
 static int hmac_test(void *p, const drew_loader_t *ldr)
@@ -337,18 +345,157 @@ static int hmac_test(void *p, const drew_loader_t *ldr)
 	return result;
 }
 
+#define MAC(x) ((drew_mac_t *)(x))
+
+int hmack_info(int op, void *p)
+{
+	return -DREW_ERR_NOT_IMPL;
+}
+
+int hmack_init(drew_kdf_t *ctx, int flags, const drew_loader_t *ldr,
+		const drew_param_t *param)
+{
+	return hmac_init(MAC(ctx), flags, ldr, param);
+}
+
+int hmack_clone(drew_kdf_t *new, const drew_kdf_t *old, int flags)
+{
+	return hmac_clone(MAC(new), MAC(old), flags);
+}
+
+int hmack_reset(drew_kdf_t *ctx)
+{
+	return hmac_reset(MAC(ctx));
+}
+
+int hmack_fini(drew_kdf_t *ctx, int flags)
+{
+	return hmac_fini(MAC(ctx), flags);
+}
+
+int hmack_setkey(drew_kdf_t *ctx, const uint8_t *key, size_t len)
+{
+	return hmac_setkey(MAC(ctx), key, len);
+}
+
+int hmack_setsalt(drew_kdf_t *ctx, const uint8_t *salt, size_t len)
+{
+	return -DREW_ERR_NOT_ALLOWED;
+}
+
+int hmack_setcount(drew_kdf_t *ctx, size_t count)
+{
+	return -DREW_ERR_NOT_ALLOWED;
+}
+
+int hmack_generate(drew_kdf_t *ctx, uint8_t *out, size_t outlen,
+		const uint8_t *in, size_t inlen)
+{
+	struct hmac *h = ctx->ctx;
+	uint8_t buf[BUFFER_SIZE];
+
+	if (outlen > h->digestsz)
+		return -DREW_ERR_INVALID;
+
+	hmac_update(MAC(ctx), in, inlen);
+	if (outlen == h->digestsz)
+		hmac_final(MAC(ctx), out, 0);
+	else {
+		hmac_final(MAC(ctx), buf, 0);
+		memcpy(out, buf, outlen);
+		memset(buf, 0, sizeof(buf));
+	}
+
+	return 0;
+}
+
+static int hmack_test_generic(const drew_loader_t *ldr, const char *name,
+		const struct test *testdata, size_t ntests, size_t outputsz)
+{
+	int result = 0;
+	drew_kdf_t c;
+	uint8_t buf[BUFFER_SIZE];
+	drew_param_t param;
+	drew_hash_t hash;
+	int id;
+
+	if ((id = drew_loader_lookup_by_name(ldr, name, 0, -1)) < 0)
+		return id;
+	drew_loader_get_functbl(ldr, id, (const void **)&hash.functbl);
+
+	hash.functbl->init(&hash, 0, ldr, NULL);
+
+	param.name = "digest";
+	param.next = NULL;
+	param.param.value = &hash;
+
+	for (size_t i = 0; i < ntests; i++) {
+		const struct test *t = testdata + i;
+		int retval;
+
+		memset(buf, 0, sizeof(buf));
+		result <<= 1;
+
+		if (t->datarep != 1)
+			continue;
+
+		hash.functbl->reset(&hash);
+		if ((retval = hmack_init(&c, 0, ldr, &param)))
+			return retval;			
+		hmack_setkey(&c, t->key, t->keysz);
+		hmack_generate(&c, buf, outputsz, t->data, t->datasz);
+
+		result |= !!memcmp(buf, t->output, outputsz);
+		hmack_fini(&c, 0);
+	}
+	hash.functbl->fini(&hash, 0);
+	
+	return result;
+}
+
+static int hmack_test_md5(const drew_loader_t *ldr, size_t *ntests)
+{
+	*ntests = DIM(testdata_md5);
+
+	return hmack_test_generic(ldr, "MD5", testdata_md5, DIM(testdata_md5), 16);
+}
+
+static int hmack_test(void *p, const drew_loader_t *ldr)
+{
+	int result = 0, tres;
+	size_t ntests = 0;
+	if (!ldr)
+		return -EINVAL;
+
+	if ((tres = hmack_test_md5(ldr, &ntests)) >= 0) {
+		result <<= ntests;
+		result |= tres;
+	}
+
+	return result;
+}
+
+
 static drew_mac_functbl_t hmac_functbl = {
 	hmac_info, hmac_init, hmac_clone, hmac_reset, hmac_fini, hmac_setkey,
 	hmac_update, hmac_update, hmac_final, hmac_test
 };
 
+static drew_kdf_functbl_t hmack_functbl = {
+	hmack_info, hmack_init, hmack_clone, hmack_reset, hmack_fini, hmack_setkey,
+	hmack_setsalt, hmack_setcount, hmack_generate, hmack_test
+};
+
 struct plugin {
 	const char *name;
-	drew_mac_functbl_t *functbl;
+	const void *functbl;
+	size_t functblsz;
+	int type;
 };
 
 static struct plugin plugin_data[] = {
-	{ "HMAC", &hmac_functbl }
+	{ "HMAC", &hmac_functbl, sizeof(drew_mac_functbl_t), DREW_TYPE_MAC },
+	{ "HMAC-KDF", &hmack_functbl, sizeof(drew_kdf_functbl_t), DREW_TYPE_KDF }
 };
 
 EXPORT()
@@ -365,11 +512,11 @@ int DREW_PLUGIN_NAME(hmac)(void *ldr, int op, int id, void *p)
 		case DREW_LOADER_GET_NPLUGINS:
 			return nplugins;
 		case DREW_LOADER_GET_TYPE:
-			return DREW_TYPE_MAC;
+			return plugin_data[id].type;
 		case DREW_LOADER_GET_FUNCTBL_SIZE:
-			return sizeof(drew_mac_functbl_t);
+			return plugin_data[id].functblsz;
 		case DREW_LOADER_GET_FUNCTBL:
-			memcpy(p, plugin_data[id].functbl, sizeof(drew_mac_functbl_t));
+			memcpy(p, plugin_data[id].functbl, plugin_data[id].functblsz);
 			return 0;
 		case DREW_LOADER_GET_NAME_SIZE:
 			return strlen(plugin_data[id].name) + 1;
