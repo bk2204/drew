40cfa3fd1f69093c685e3df2890d6d3f5629fe54
brian m. carlson <sandals@crustytoothpaste.net>
2011-07-30 21:20:44 +0000

    Add permission notices to more files.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- test/plugin-main.c
+++ test/plugin-main.c
@@ -1,3 +1,9 @@
+/*-
+ * brian m. carlson <sandals@crustytoothpaste.ath.cx> wrote this source code.
+ * This source code is in the public domain; you may do whatever you please with
+ * it.  However, a credit in the documentation, although not required, would be
+ * appreciated.
+ */
 #include <drew/plugin.h>
 
 #include <errno.h>
--- util/sha224sum.c
+++ util/sha224sum.c
@@ -1,3 +1,9 @@
+/*-
+ * brian m. carlson <sandals@crustytoothpaste.ath.cx> wrote this source code.
+ * This source code is in the public domain; you may do whatever you please with
+ * it.  However, a credit in the documentation, although not required, would be
+ * appreciated.
+ */
 #define ALGO_PLUGIN_NAME	"sha256"
 #define ALGO_NAME			"SHA-224"
 #define ALGO_BLOCK_SIZE		64
