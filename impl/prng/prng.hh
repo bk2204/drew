ef9c694e8626470a273a5ea9bf74e2a4b68767b2
brian m. carlson <sandals@crustytoothpaste.net>
2011-08-02 21:28:09 +0000

    Add system PRNGs.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- impl/prng/prng.hh
+++ impl/prng/prng.hh
@@ -21,9 +21,10 @@
 #define BMC_PRNG_HH
 
 #include <internal.h>
-#include "util.h"
+#include "util.hh"
 #include <drew/prng.h>
 
+#include <errno.h>
 #include <fcntl.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -127,19 +128,153 @@ class SeedlessPRNG : public virtual PRNG
 	private:
 };
 
-class DevURandom : public SeedlessPRNG, public BlockPRNG
+class SystemPRNG : public SeedlessPRNG, public BlockPRNG
 {
 	public:
-		void GetBytes(uint8_t *buf, size_t nbytes)
+		virtual int GetBytes(uint8_t *buf, size_t nbytes) = 0;
+		virtual int CheckImplementation() = 0;
+};
+
+class RandomDevice : public SystemPRNG
+{
+	public:
+		virtual int GetBytes(uint8_t *buf, size_t nbytes)
 		{
-			int fd = open(RANDOM_DEVICE, O_RDONLY);
-			read(fd, buf, nbytes);
+			ssize_t sz;
+			if (!filename)
+				return -DREW_ERR_NOT_IMPL;
+			int fd = open(filename, O_RDONLY), err = 0;
+			if (fd < 0)
+				return -errno;
+			sz = read(fd, buf, nbytes);
+			err = -errno;
 			close(fd);
+			return sz < 0 ? err : sz;
+		}
+		virtual int CheckImplementation()
+		{
+			if (!filename)
+				return -DREW_ERR_NOT_IMPL;
+			int fd = open(filename, O_RDONLY);
+			if (fd < 0) {
+				if (errno == ENOENT)
+					return -DREW_ERR_NOT_IMPL;
+				else
+					return -errno;
+			}
+			close(fd);
+			return 0;
+		}
+		using SeedlessPRNG::AddRandomData;
+	protected:
+		RandomDevice() : filename(NULL) {}
+		const char *filename;
+		void Stir() {}
+};
+
+class DevURandom : public RandomDevice
+{
+	public:
+		DevURandom() { filename = "/dev/urandom"; }
+};
+
+class DevRandom : public RandomDevice
+{
+	public:
+		DevRandom()
+		{
+#ifdef __OpenBSD__
+			filename = "/dev/srandom";
+#else
+			filename = "/dev/random";
+#endif
+		}
+};
+
+class DevHWRandom : public RandomDevice
+{
+	public:
+		DevHWRandom()
+		{
+#ifdef __OpenBSD__
+			filename = "/dev/random";
+#elif __linux__
+			filename = "/dev/hwrng";
+#endif
+		}
+};
+
+template<class T, class U>
+class FallbackPRNG : public SystemPRNG
+{
+	public:
+		virtual int GetBytes(uint8_t *buf, size_t nbytes)
+		{
+			int res = 0;
+			if (tprng.CheckImplementation())
+				return uprng.GetBytes(buf, nbytes);
+			res = tprng.GetBytes(buf, nbytes);
+			if (res < 0)
+				return uprng.GetBytes(buf, nbytes);
+			return res;
+		}
+		virtual int CheckImplementation()
+		{
+			int res = tprng.CheckImplementation();
+			if (res)
+				return uprng.CheckImplementation();
+			return res;
+		}
+		using SeedlessPRNG::AddRandomData;
+	private:
+		T tprng;
+		U uprng;
+};
+
+#ifdef __RDRND__
+class RDRAND : public SystemPRNG
+{
+	public:
+		static const size_t chunksz = sizeof(void *);
+		int GetBytes(uint8_t *buf, size_t nbytes)
+		{
+			int total = 0;
+			for (size_t i = 0; i < DivideAndRoundUp(nbytes, chunksz);
+					i++, total += chunksz, buf += chunksz) {
+				if (chunksz == 4) {
+					uint32_t val32;
+					if (!__builtin_ia32_rdrand32_step(&val32))
+						break;
+					memcpy(buf, &val32, std::min<size_t>(chunksz,
+								nbytes - total));
+				}
+				else {
+					unsigned long long val64;
+					if (!__builtin_ia32_rdrand64_step(&val64))
+						break;
+					memcpy(buf, &val64, std::min<size_t>(chunksz,
+								nbytes - total));
+				}
+			}
+			if (size_t(total) > nbytes)
+				total = nbytes;
+			if (!total)
+				return -DREW_ERR_FAILED;
+			return total;
+		}
+		int CheckImplementation()
+		{
+			uint32_t a, b, c, d;
+			GetCpuid(1, a, b, c, d);
+			return c & 0x40000000 ? 0 : -DREW_ERR_NOT_IMPL;
 		}
 		using SeedlessPRNG::AddRandomData;
 	protected:
 		void Stir() {}
 };
+#endif
+typedef DevURandom ReliablePRNG;
+
 
 }
 UNHIDE()
