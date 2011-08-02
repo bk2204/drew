7346027625e72564dfc559a98c7e76ffeebd1960
brian m. carlson <sandals@crustytoothpaste.net>
2011-08-02 20:42:40 +0000

    Fix querying a zero number of implementations.
    A plugin can contain zero implementations; this is common when the hardware does
    not support the functionality required of the plugin.  We should return zero
    when querying the number of implementations in such a plugin.  Previously, we
    did not allocate an ID for such dummy plugins, so querying the number of
    implementations would return an incorrect value.  Now, we allocate an ID that is
    useless for anything else but can be queried for the number of plugins.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- lib/libdrew/drew.c
+++ lib/libdrew/drew.c
@@ -33,6 +33,8 @@
 
 // Set if the plugin has been properly loaded.
 #define FLAG_PLUGIN_OK		1
+// Set if the plugin contains no implementations.
+#define FLAG_PLUGIN_DUMMY	2
 
 typedef int (*plugin_api_t)(void *, int, int, void *);
 
@@ -103,6 +105,7 @@ static int load_library(drew_loader_t *ldr, const char *library,
 		return -ENOMEM;
 	ldr->lib = p;
 	lib = &ldr->lib[ldr->nlibs];
+	memset(lib, 0, sizeof(*lib));
 	ldr->nlibs++;
 
 	if (!library) {
@@ -150,18 +153,26 @@ static int load_library_info(drew_loader_t *ldr, library_t *lib)
 {
 	int err = -DREW_ERR_ENUMERATION;
 	int offset = ldr->nplugins;
+	int extra = 0;
 	plugin_t *p = NULL;
 
 	lib->nplugins = lib->api(ldr, DREW_LOADER_GET_NPLUGINS, 0, NULL);
 	if (lib->nplugins < 0)
 		goto out;
 
+	if (!lib->nplugins)
+		extra = 1;
+
 	err = -ENOMEM;
 	p = realloc(ldr->plugin, sizeof(*p) * (ldr->nplugins + lib->nplugins + 1));
 	if (!p)
 		goto out;
-	ldr->nplugins += lib->nplugins;
+	memset(p+ldr->nplugins, 0, sizeof(*p) * (lib->nplugins + 1));
+	ldr->nplugins += lib->nplugins + extra;
 	ldr->plugin = p;
+
+	if (!lib->nplugins)
+		p[ldr->nplugins-1].flags = FLAG_PLUGIN_DUMMY;
 
 	p += offset;
 	for (int i = 0; i < lib->nplugins; i++, p++) {
@@ -297,15 +308,16 @@ int drew_loader_load_plugin(drew_loader_t *ldr, const char *plugin,
 	return load_library_info(ldr, lib);
 }
 
-static inline bool is_valid_id(const drew_loader_t *ldr, int id)
+static inline bool is_valid_id(const drew_loader_t *ldr, int id, int dummyok)
 {
+	int mask = FLAG_PLUGIN_OK | (dummyok ? FLAG_PLUGIN_DUMMY : 0);
 	if (!ldr)
 		return false;
 	if (id < 0)
 		return false;
 	if (id >= ldr->nplugins)
 		return false;
-	if (!(ldr->plugin[id].flags & FLAG_PLUGIN_OK))
+	if (!(ldr->plugin[id].flags & mask))
 		return false;
 	return true;
 }
@@ -320,8 +332,12 @@ int drew_loader_get_nplugins(const drew_loader_t *ldr, int id)
 		return -DREW_ERR_INVALID;
 	if (id == -1)
 		return ldr->nplugins;
-	if (!is_valid_id(ldr, id))
-		return -DREW_ERR_INVALID;
+	if (!is_valid_id(ldr, id, 0)) {
+		if (is_valid_id(ldr, id, 1))
+			return 0;
+		else
+			return -DREW_ERR_INVALID;
+	}
 
 	return ldr->plugin[id].lib->nplugins;
 }
@@ -330,7 +346,7 @@ int drew_loader_get_type(const drew_loader_t *ldr, int id)
 {
 	if (!ldr)
 		return -DREW_ERR_INVALID;
-	if (!is_valid_id(ldr, id))
+	if (!is_valid_id(ldr, id, 0))
 		return -DREW_ERR_INVALID;
 
 	return ldr->plugin[id].type;
@@ -340,7 +356,7 @@ int drew_loader_get_functbl(const drew_loader_t *ldr, int id, const void **tbl)
 {
 	if (!ldr)
 		return -DREW_ERR_INVALID;
-	if (!is_valid_id(ldr, id))
+	if (!is_valid_id(ldr, id, 0))
 		return -DREW_ERR_INVALID;
 
 	if (tbl)
@@ -353,7 +369,7 @@ int drew_loader_get_algo_name(const drew_loader_t *ldr, int id,
 {
 	if (!ldr)
 		return -DREW_ERR_INVALID;
-	if (!is_valid_id(ldr, id))
+	if (!is_valid_id(ldr, id, 0))
 		return -DREW_ERR_INVALID;
 
 	*namep = ldr->plugin[id].name;
@@ -369,7 +385,7 @@ int drew_loader_lookup_by_name(const drew_loader_t *ldr, const char *name,
 		end = ldr->nplugins;
 
 	for (int i = start; i < end; i++) {
-		if (!is_valid_id(ldr, i))
+		if (!is_valid_id(ldr, i, 0))
 			continue;
 		if (!strcmp(ldr->plugin[i].name, name))
 			return i;
@@ -387,7 +403,7 @@ int drew_loader_lookup_by_type(const drew_loader_t *ldr, int type, int start,
 		end = ldr->nplugins;
 
 	for (int i = start; i < end; i++) {
-		if (!is_valid_id(ldr, i))
+		if (!is_valid_id(ldr, i, 0))
 			continue;
 		if (ldr->plugin[i].type == type)
 			return i;
@@ -464,7 +480,7 @@ int drew_loader_get_metadata(const drew_loader_t *ldr, int id, int item,
 	if (!ldr)
 		return -DREW_ERR_INVALID;
 
-	if (!is_valid_id(ldr, id))
+	if (!is_valid_id(ldr, id, 0))
 		return -DREW_ERR_NONEXISTENT;
 
 	if (item == -1) {
