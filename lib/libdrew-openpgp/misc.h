95f7004e86b6d6a2e514a7a7c6d115ede91a814f
brian m. carlson <sandals@crustytoothpaste.net>
2011-08-05 23:37:10 +0000

    Export key component free functions to library.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- lib/libdrew-openpgp/misc.h
+++ lib/libdrew-openpgp/misc.h
@@ -15,6 +15,11 @@ int clone_subpackets(drew_opgp_subpacket_group_t *,
 int clone_sig(csig_t *, csig_t *);
 int clone_uid(cuid_t *, cuid_t *);
 int clone_pubkey(pubkey_t *, pubkey_t *, pubkey_t *parent);
+void free_mpi(drew_opgp_mpi_t *mpi);
+void free_subpacket_group(drew_opgp_subpacket_group_t *spg);
+void free_sig(csig_t *sig);
+void free_uid(cuid_t *uid);
+void free_pubkey(pubkey_t *pub);
 UNHIDE()
 
 #ifdef __cplusplus
