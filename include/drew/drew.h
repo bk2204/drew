c83b104e0bea0e3ccba77bac32c196df36abbae8
brian m. carlson <sandals@crustytoothpaste.net>
2011-08-13 00:57:11 +0000

    Add support in Drew for hidden visibility.
    C++ code, even with visibility pragmas, tends to leak symbols.  This commit
    allows appropriately marking functions as exportable, so that the remainder can
    be marked hidden.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- include/drew/drew.h
+++ include/drew/drew.h
@@ -22,6 +22,43 @@
 #ifndef DREW_DREW_H
 #define DREW_DREW_H
 
+/* Not that this will work in Win32 without a decent amount of work, but hey...
+ *
+ * Code pulled from the GCC wiki.
+ */
+#if defined(_WIN32) || defined(__CYGWIN__)
+#ifdef DREW_IN_BUILD
+
+#ifdef __GNUC__
+#define DREW_SYM_PUBLIC __attribute__ ((dllexport))
+#else
+#define DREW_SYM_PUBLIC __declspec(dllexport)
+#endif
+
+#else
+
+#ifdef __GNUC__
+#define DREW_SYM_PUBLIC __attribute__ ((dllimport))
+#else
+#define DREW_SYM_PUBLIC __declspec(dllimport)
+#endif
+
+#endif
+
+#define DREW_SYM_HIDDEN
+
+#else
+
+#if defined(__GNUC__) && __GNUC__ >= 4
+#define DREW_SYM_PUBLIC __attribute__ ((visibility("default")))
+#define DREW_SYM_HIDDEN __attribute__ ((visibility("hidden")))
+#else
+#define DREW_SYM_PUBLIC
+#define DREW_SYM_HIDDEN
+#endif
+
+#endif
+
 #include <drew/plugin.h>
 
 #endif
