b15f16169beabb1f54423c7425a1209d8e8e39c7
brian m. carlson <sandals@crustytoothpaste.net>
2011-10-03 19:36:04 +0000

    gcm: use aligned operations when possible.
    This patch cleans up some of the optimized code and also makes use of aligned
    XORs for better efficiency.  This improves speed from 69 MiB/s to 75 MiB/s.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- impl/mode/gcm.cc
+++ impl/mode/gcm.cc
@@ -227,36 +227,33 @@ static inline void mul(struct gcm *ctx, uint8_t *buf)
 /* The 64k table implementation is derived from Crypto++. */
 static inline void mul_fl(struct gcm *ctx, uint8_t *buf)
 {
-	uint64_t x[2], a[2] = {0, 0};
+	uint64_t x[2], a[2] ALIGNED_T = {0, 0};
 
 	memcpy(x, buf, 16);
-#define READ_COMMON(a, c, d) (*(uint64_t *)(ctx->table+(a)*256*16+(c)+(d)*8))
+#define PTR_COMMON(a, c) ((uint64_t *)(ctx->table+(a)*256*16+(c)))
 #if DREW_BYTE_ORDER == DREW_LITTLE_ENDIAN
 #define RE(c, d) (d+4*(c%2))
 #else
 #define RE(c, d) (7-d-4*(c%2))
 #endif
-#define READ_WORD(b, c, d, e) READ_COMMON(c*4+d, (RE(c, d)?(x[b]>>((RE(c, d)?RE(c, d):1)*8-4))&0xff0:(x[b]&0xff)<<4), e)
-#define MUL_8BY128(op, b, c, d) \
-	a[0] op READ_WORD(b, c, d, 0); \
-	a[1] op READ_WORD(b, c, d, 1);
+#define PTR_WORD(b, c, d) PTR_COMMON(c*4+d, (RE(c, d)?(x[b]>>((RE(c, d)?RE(c, d):1)*8-4))&0xff0:(x[b]&0xff)<<4))
 
-	MUL_8BY128(^=, 0, 0, 0);
-	MUL_8BY128(^=, 0, 0, 1);
-	MUL_8BY128(^=, 0, 0, 2);
-	MUL_8BY128(^=, 0, 0, 3);
-	MUL_8BY128(^=, 0, 1, 0);
-	MUL_8BY128(^=, 0, 1, 1);
-	MUL_8BY128(^=, 0, 1, 2);
-	MUL_8BY128(^=, 0, 1, 3);
-	MUL_8BY128(^=, 1, 2, 0);
-	MUL_8BY128(^=, 1, 2, 1);
-	MUL_8BY128(^=, 1, 2, 2);
-	MUL_8BY128(^=, 1, 2, 3);
-	MUL_8BY128(^=, 1, 3, 0);
-	MUL_8BY128(^=, 1, 3, 1);
-	MUL_8BY128(^=, 1, 3, 2);
-	MUL_8BY128(^=, 1, 3, 3);
+	XorAligned(a, PTR_WORD(0, 0, 0), 16);
+	XorAligned(a, PTR_WORD(0, 0, 1), 16);
+	XorAligned(a, PTR_WORD(0, 0, 2), 16);
+	XorAligned(a, PTR_WORD(0, 0, 3), 16);
+	XorAligned(a, PTR_WORD(0, 1, 0), 16);
+	XorAligned(a, PTR_WORD(0, 1, 1), 16);
+	XorAligned(a, PTR_WORD(0, 1, 2), 16);
+	XorAligned(a, PTR_WORD(0, 1, 3), 16);
+	XorAligned(a, PTR_WORD(1, 2, 0), 16);
+	XorAligned(a, PTR_WORD(1, 2, 1), 16);
+	XorAligned(a, PTR_WORD(1, 2, 2), 16);
+	XorAligned(a, PTR_WORD(1, 2, 3), 16);
+	XorAligned(a, PTR_WORD(1, 3, 0), 16);
+	XorAligned(a, PTR_WORD(1, 3, 1), 16);
+	XorAligned(a, PTR_WORD(1, 3, 2), 16);
+	XorAligned(a, PTR_WORD(1, 3, 3), 16);
 	memcpy(buf, a, 16);
 }
 
