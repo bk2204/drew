7b99df1348488ff9b7e4da51280d1695b5540503
brian m. carlson <sandals@crustytoothpaste.net>
2011-11-01 20:56:54 +0000

    cast6: update for ABI 3.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- impl/block/cast/cast6.hh
+++ impl/block/cast/cast6.hh
@@ -35,10 +35,10 @@ class CAST6 : public CAST, public BlockCipher<16, BigEndian>
 	public:
 		CAST6();
 		~CAST6() {};
-		int SetKey(const uint8_t *key, size_t sz);
 		int Encrypt(uint8_t *out, const uint8_t *in) const;
 		int Decrypt(uint8_t *out, const uint8_t *in) const;
 	protected:
+		int SetKeyInternal(const uint8_t *key, size_t sz);
 	private:
 		uint32_t m_km[4][12];
 		uint8_t m_kr[4][12];
