c5fda9b5ffc89a60aed6e159939f6dc55b1c86cd
brian m. carlson <sandals@crustytoothpaste.net>
2011-11-12 00:10:11 +0000

    Add MARS.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

---
+++ impl/block/mars/mars.hh
@@ -0,0 +1,52 @@
+/*-
+ * Copyright © 2011 brian m. carlson
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#ifndef MARS_HH
+#define MARS_HH
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "block-plugin.hh"
+#include "util.hh"
+
+HIDE()
+namespace drew {
+
+class MARS : public BlockCipher<16, LittleEndian>
+{
+	public:
+		MARS();
+		~MARS() {};
+		int Encrypt(uint8_t *out, const uint8_t *in) const;
+		int Decrypt(uint8_t *out, const uint8_t *in) const;
+	protected:
+		int SetKeyInternal(const uint8_t *key, size_t sz);
+		static void e(uint32_t &l, uint32_t &m, uint32_t &r, uint32_t in,
+				uint32_t k1, uint32_t k2);
+	private:
+		uint32_t m_k[40];
+};
+
+}
+UNHIDE()
+
+#endif
