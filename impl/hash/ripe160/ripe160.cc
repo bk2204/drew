a760ab7f47e66cd1eddc7cf9d6a9b7b6a5cb5160
brian m. carlson <sandals@crustytoothpaste.net>
2011-12-03 18:32:27 +0000

    Implement RIPEMD-320.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- impl/hash/ripe160/ripe160.cc
+++ impl/hash/ripe160/ripe160.cc
@@ -22,8 +22,10 @@
 HIDE()
 extern "C" {
 PLUGIN_STRUCTURE(rmd160, RIPEMD160)
+PLUGIN_STRUCTURE(rmd320, RIPEMD320)
 PLUGIN_DATA_START()
 PLUGIN_DATA(rmd160, "RIPEMD-160")
+PLUGIN_DATA(rmd320, "RIPEMD-320")
 PLUGIN_DATA_END()
 PLUGIN_INTERFACE(ripe160)
 
@@ -48,6 +50,29 @@ static int rmd160test(void *, const drew_loader_t *)
 	res |= !HashTestCase<RIPEMD160>("12345678901234567890123456789012345678901234567890123456789012345678901234567890", 1).Test("9b752e45573d4b39f4dbd3323cab82bf63326bfb");
 	res <<= 1;
 	res |= !HashTestCase<RIPEMD160>::MaintenanceTest("73154a71f6286c75073dfba04128de5b074d8cdf");
+
+	return res;
+}
+
+static int rmd320test(void *, const drew_loader_t *)
+{
+	int res = 0;
+
+	using namespace drew;
+
+	res |= !HashTestCase<RIPEMD320>("", 0).Test("22d65d5661536cdc75c1fdf5c6de7b41b9f27325ebc61e8557177d705a0ec880151c3a32a00899b8");
+	res <<= 1;
+	res |= !HashTestCase<RIPEMD320>("a", 1).Test("ce78850638f92658a5a585097579926dda667a5716562cfcf6fbe77f63542f99b04705d6970dff5d");
+	res <<= 1;
+	res |= !HashTestCase<RIPEMD320>("abc", 1).Test("de4c01b3054f8930a79d09ae738e92301e5a17085beffdc1b8d116713e74f82fa942d64cdbc4682d");
+	res <<= 1;
+	res |= !HashTestCase<RIPEMD320>("message digest", 1).Test("3a8e28502ed45d422f68844f9dd316e7b98533fa3f2a91d29f84d425c88d6b4eff727df66a7c0197");
+	res <<= 1;
+	res |= !HashTestCase<RIPEMD320>("abcdefghijklmnopqrstuvwxyz", 1).Test("cabdb1810b92470a2093aa6bce05952c28348cf43ff60841975166bb40ed234004b8824463e6b009");
+	res <<= 1;
+	res |= !HashTestCase<RIPEMD320>("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", 1).Test("ed544940c86d67f250d232c30b7b3e5770e0c60c8cb9a4cafe3b11388af9920e1b99230b843c86a4");
+	res <<= 1;
+	res |= !HashTestCase<RIPEMD320>("12345678901234567890123456789012345678901234567890123456789012345678901234567890", 1).Test("557888af5f6d8ed62ab66945c6d2a0a47ecd5341e915eb8fea1d0524955f825dc717e4a008ab2d42");
 
 	return res;
 }
@@ -165,5 +190,88 @@ void drew::RIPEMD160::Transform(quantum_t *state, const uint8_t *block)
 	state[3] = state[4] + a + bb;
 	state[4] = state[0] + b + cc;
 	state[0] = t;
+}
+
+drew::RIPEMD320::RIPEMD320()
+{
+	Reset();
+}
+
+void drew::RIPEMD320::Reset()
+{
+	m_hash[0] = 0x67452301;
+	m_hash[1] = 0xefcdab89;
+	m_hash[2] = 0x98badcfe;
+	m_hash[3] = 0x10325476;
+	m_hash[4] = 0xc3d2e1f0;
+	m_hash[5] = 0x76543210;
+	m_hash[6] = 0xfedcba98;
+	m_hash[7] = 0x89abcdef;
+	m_hash[8] = 0x01234567;
+	m_hash[9] = 0x3c2d1e0f;
+	Initialize();
+}
+
+#define STD(a, b, c, d, e) a=e; e=d; d=RotateLeft(c, 10); c=b; b=t;
+#define OP(f, r, s, k, a, b, c, d, e) \
+	t=RotateLeft(a+f(b, c, d)+blk[r[i]]+k, s[i])+e; \
+	STD(a, b, c, d, e);
+
+void drew::RIPEMD320::Transform(quantum_t *state, const uint8_t *block)
+{
+	uint32_t buf[block_size/sizeof(uint32_t)];
+	const uint32_t *blk;
+	size_t i;
+	uint32_t a, b, c, d, e, aa, bb, cc, dd, ee, t;
+
+	a = state[0];
+	b = state[1];
+	c = state[2];
+	d = state[3];
+	e = state[4];
+	aa = state[5];
+	bb = state[6];
+	cc = state[7];
+	dd = state[8];
+	ee = state[9];
+
+	blk = endian_t::CopyIfNeeded(buf, block, block_size);
+
+	for (i=0; i<16; i++) {
+		OP(ff,  r,  s, 0x00000000,  a,  b,  c,  d,  e);
+		OP(jj, rp, sp, 0x50a28be6, aa, bb, cc, dd, ee);
+	}
+	std::swap(b, bb);
+	for (i=16; i<32; i++) {
+		OP(gg,  r,  s, 0x5a827999,  a,  b,  c,  d,  e);
+		OP(ii, rp, sp, 0x5c4dd124, aa, bb, cc, dd, ee);
+	}
+	std::swap(d, dd);
+	for (i=32; i<48; i++) {
+		OP(hh,  r,  s, 0x6ed9eba1,  a,  b,  c,  d,  e);
+		OP(hh, rp, sp, 0x6d703ef3, aa, bb, cc, dd, ee);
+	}
+	std::swap(a, aa);
+	for (i=48; i<64; i++) {
+		OP(ii,  r,  s, 0x8f1bbcdc,  a,  b,  c,  d,  e);
+		OP(gg, rp, sp, 0x7a6d76e9, aa, bb, cc, dd, ee);
+	}
+	std::swap(c, cc);
+	for (i=64; i<80; i++) {
+		OP(jj,  r,  s, 0xa953fd4e,  a,  b,  c,  d,  e);
+		OP(ff, rp, sp, 0x00000000, aa, bb, cc, dd, ee);
+	}
+	std::swap(e, ee);
+
+	state[0] += a;
+	state[1] += b;
+	state[2] += c;
+	state[3] += d;
+	state[4] += e;
+	state[5] += aa;
+	state[6] += bb;
+	state[7] += cc;
+	state[8] += dd;
+	state[9] += ee;
 }
 UNHIDE()
--- impl/hash/ripe160/ripe160.hh
+++ impl/hash/ripe160/ripe160.hh
@@ -29,6 +29,21 @@ class RIPEMD160 : public Hash<uint32_t, 20, 20, 64, LittleEndian>
 	private:
 };
 
+class RIPEMD320 : public Hash<uint32_t, 40, 40, 64, LittleEndian>
+{
+	public:
+		RIPEMD320();
+		virtual ~RIPEMD320() {}
+		void Reset();
+		static void Transform(quantum_t *state, const uint8_t *data);
+	protected:
+		virtual void Transform(const uint8_t *data)
+		{
+			Transform(m_hash, data);
+		}
+	private:
+};
+
 }
 UNHIDE()
 
