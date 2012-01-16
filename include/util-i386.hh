b8c2e05b8e7bc5bf3176145bb54bd411429c8d1d
brian m. carlson <sandals@crustytoothpaste.net>
2012-01-16 22:08:39 +0000

    util-i386: implement endianness code for vectors.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- include/util-i386.hh
+++ include/util-i386.hh
@@ -94,6 +94,36 @@ inline uint8_t EndianBase::GetArrayByte(const uint64_t *arr, size_t n)
 }
 
 #if defined(DREW_COMPILER_GCCLIKE) && defined(__SSSE3__) && defined(VECTOR_T)
+typedef long long int drew__vector64_t __attribute__((vector_size(16)));
+
+template<>
+template<>
+inline uint8_t *Endian<DREW_BIG_ENDIAN>::Copy<drew__vector64_t>(uint8_t *dest, const drew__vector64_t *src, size_t len)
+{
+	typedef char vector_t __attribute__ ((vector_size (16)));
+	const vector_t perm = {0x0f, 0x0e, 0x0d, 0x0c, 0x0b, 0x0a, 0x09, 0x08, 0x07, 0x06, 0x05, 0x04, 0x03, 0x02, 0x01, 0x00};
+	for (size_t i = 0, j = 0; i < len; i += 16, j++) {
+		vector_t buf = vector_t(src[j]);
+		buf = __builtin_ia32_pshufb128(buf, perm);
+		memcpy(dest+i, &buf, sizeof(buf));
+	}
+	return dest;
+}
+template<>
+template<>
+inline drew__vector64_t *Endian<DREW_BIG_ENDIAN>::Copy<drew__vector64_t>(drew__vector64_t *dest, const uint8_t *src, size_t len)
+{
+	typedef char vector_t __attribute__ ((vector_size (16)));
+	const vector_t perm = {0x0f, 0x0e, 0x0d, 0x0c, 0x0b, 0x0a, 0x09, 0x08, 0x07, 0x06, 0x05, 0x04, 0x03, 0x02, 0x01, 0x00};
+	for (size_t i = 0, j = 0; i < len; i += 16, j++) {
+		vector_t buf;
+		memcpy(&buf, src+i, sizeof(buf));
+		buf = __builtin_ia32_pshufb128(buf, perm);
+		dest[j] = drew__vector64_t(buf);
+	}
+	return dest;
+}
+
 template<>
 template<>
 inline uint8_t *Endian<DREW_BIG_ENDIAN>::Copy<uint32_t>(uint8_t *dest, const uint32_t *src, size_t len)
