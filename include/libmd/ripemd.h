b55806686d3425da5f6b038a4b845a8e2270f1c7
brian m. carlson <sandals@crustytoothpaste.net>
2012-03-23 21:57:33 +0000

    libmd: provide dummy headers with FreeBSD names.
    FreeBSD uses different header names than NetBSD and OpenBSD do, so provide
    headers by those names that simply include the appropriate header.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

---
+++ include/libmd/ripemd.h
@@ -0,0 +1,6 @@
+#ifndef DREW_LIBMD_RMD160_H
+#define DREW_LIBMD_RMD160_H
+
+#include <libmd/ripe160.h>
+
+#endif
---
+++ include/libmd/sha1.h
@@ -0,0 +1,6 @@
+#ifndef DREW_LIBMD_SHA1_H
+#define DREW_LIBMD_SHA1_H
+
+#include <libmd/sha.h>
+
+#endif
