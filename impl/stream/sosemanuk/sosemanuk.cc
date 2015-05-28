fb93b04ddf2505dd3478350fe76e22489c5e7114
brian m. carlson <sandals@crustytoothpaste.net>
2015-05-28 23:48:00 +0000

    sosemanuk: remove unused array.
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- impl/stream/sosemanuk/sosemanuk.cc
+++ impl/stream/sosemanuk/sosemanuk.cc
@@ -89,7 +89,6 @@ static const int sosemanuk_keysz[] = {
 	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
 	17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32
 };
-static const int sosemanuk_ivsiz[] = {16};
 
 static int sosemanuk_info(int op, void *p)
 {
